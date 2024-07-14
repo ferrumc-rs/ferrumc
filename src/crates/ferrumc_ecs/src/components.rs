bce121c0e9e110f4bab20d9eec1d59306c0da049
Sweattypalms <stranger8722@gmail.com>
2024-07-14 13:13:24 +0900

    macros i think

--- src/crates/ferrumc_ecs/src/components.rs
+++ src/crates/ferrumc_ecs/src/components.rs
@@ -1,26 +1,19 @@
-use std::any::Any;
-
-#[derive(Debug, Clone, Copy, Default)]
+#[derive(Debug, Default)]
 pub struct Position {
     pub x: f32,
     pub y: f32,
     pub z: f32,
 }
 
-#[derive(Debug, Clone, Copy, Default)]
+#[derive(Debug, Default)]
 pub struct Velocity {
     pub x: f32,
     pub y: f32,
     pub z: f32,
 }
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Default)]
 pub struct Player {
     pub name: String,
     pub health: i32,
 }
-
-/// A marker trait for components.
-/// By implementing this trait, a type can be used as a component in the ECS.
-pub trait Component: Any + 'static {}
-impl<T: Any + 'static> Component for T {}
--- src/crates/ferrumc_ecs/src/world.rs
+++ src/crates/ferrumc_ecs/src/world.rs
@@ -1,92 +1,82 @@
-use std::any::{Any, TypeId};
-use std::collections::HashMap;
-use crate::components::Component;
+use crate::components::{Position, Velocity};
 
-pub type Entity = u32;
+pub type Entity = u64;
 
-/// Basically a storage for components of a specific type. <br/>
-/// <b>data</b>: A vector of boxed components.
-/// <b>component_type</b>: The type of the component.
-pub struct ComponentVec {
-    /// Basically a pointer to a heap-allocated object.
-    data: Vec<Box<dyn Any>>,
-    component_type: TypeId,
+pub struct ComponentArray<T> {
+    data: Vec<Option<T>>,
 }
 
-impl ComponentVec {
-    pub fn new<T: 'static>() -> Self {
-        ComponentVec {
-            data: Vec::new(),
-            component_type: TypeId::of::<T>(),
+impl<T> ComponentArray<T> {
+    fn new() -> Self {
+        ComponentArray { data: Vec::new() }
+    }
+
+    fn insert(&mut self, index: usize, component: T) {
+        if index >= self.data.len() {
+            self.data.resize_with(index + 1, || None);
+        }
+        self.data[index] = Some(component);
+    }
+
+    fn get(&self, index: usize) -> Option<&T> {
+        self.data.get(index).and_then(|opt| opt.as_ref())
+    }
+
+    fn get_mut(&mut self, index: usize) -> Option<&mut T> {
+        self.data.get_mut(index).and_then(|opt| opt.as_mut())
+    }
+
+    fn get_all<'a>(&mut self) -> &'a Vec<Option<T>> {
+        &self.data
+    }
+}
+
+macro_rules! define_world {
+    ($($component:ident),*) => {
+        #[derive(Default)]
+        pub struct World {
+            entities: Vec<Entity>,
+            next_entity_id: Entity,
+            $(
+                $component: ComponentArray<$component>,
+            )*
+        }
+
+        impl World {
+            pub fn new() -> Self {
+                World::default()
+            }
+
+            pub fn new_entity(&mut self) -> Entity {
+                let entity = self.next_entity_id;
+                self.next_entity_id += 1;
+                self.entities.push(entity);
+                entity
+            }
+
+            $(
+                pub fn add_component_$component(&mut self, entity: Entity, component: $component) {
+                    let index = self.entities.iter().position(|&e| e == entity).unwrap();
+                    self.$component.insert(index, component);
+                }
+
+                pub fn get_component_$component(&self, entity: Entity) -> Option<&$component> {
+                    let index = self.entities.iter().position(|&e| e == entity)?;
+                    self.$component.get(index)
+                }
+
+                pub fn get_component_mut_$component(&mut self, entity: Entity) -> Option<&mut $component> {
+                    let index = self.entities.iter().position(|&e| e == entity)?;
+                    self.$component.get_mut(index)
+                }
+
+                pub fn get_all_component_$component<'a>(&mut self) -> &'a Vec<$component> {
+                    $component.get_all()
+                }
+            )*
         }
     }
-
-    pub fn push<T: 'static>(&mut self, component: T) {
-        self.data.push(Box::new(component));
-    }
-
-    pub fn get<T: 'static>(&self, index: usize) -> Option<&T> {
-        self.data.get(index)?.downcast_ref()
-    }
-
-    pub fn get_mut<T: 'static>(&mut self, index: usize) -> Option<&mut T> {
-        self.data.get_mut(index)?.downcast_mut()
-    }
 }
 
-pub struct World {
-    entities: Vec<Entity>,
-    components: HashMap<TypeId, ComponentVec>,
-    entity_to_index: HashMap<Entity, usize>,
-    next_entity_id: Entity,
-}
-
-impl World {
-    pub fn new() -> Self {
-        World {
-            entities: Vec::new(),
-            components: HashMap::new(),
-            entity_to_index: HashMap::new(),
-            next_entity_id: 0,
-        }
-    }
-
-    pub fn new_entity(&mut self) -> Entity {
-        let entity = self.next_entity_id;
-        self.next_entity_id += 1;
-        self.entities.push(entity);
-        let index = self.entities.len() - 1;
-        self.entity_to_index.insert(entity, index);
-        entity
-    }
-
-    pub fn add_component<T: Component + Default>(&mut self, entity: Entity, component: T) {
-        let type_id = TypeId::of::<T>();
-        let index = *self.entity_to_index.get(&entity).unwrap();
-
-        self.components
-            .entry(type_id)
-            .or_insert_with(|| ComponentVec::new::<T>())
-            .push(component);
-
-        while self.components.get(&type_id).unwrap().data.len() <= index {
-            self.components.get_mut(&type_id).unwrap().push(T::default());
-        }
-    }
-
-    pub fn get_component<T: Component>(&self, entity: Entity) -> Option<&T> {
-        let type_id = TypeId::of::<T>();
-        let index = *self.entity_to_index.get(&entity)?;
-        self.components.get(&type_id)?.get(index)
-    }
-
-    pub fn get_component_mut<T: Component>(&mut self, entity: Entity) -> Option<&mut T> {
-        let type_id = TypeId::of::<T>();
-        let index = *self.entity_to_index.get(&entity)?;
-        self.components.get_mut(&type_id)?.get_mut(index)
-    }
-
-    pub fn entities(&self) -> &[Entity] {
-        &self.entities
-    }
-}
+// Use the macro to define the World struct with these components
+define_world!(Position,Velocity);
