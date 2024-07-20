b368784d9bae92f1c0afd11f272656b5a3eff0d9
Sweattypalms <stranger8722@gmail.com>
2024-07-20 20:29:04 +0900

    Mutable queries

--- src/crates/ferrumc_ecs/src/components.rs
+++ src/crates/ferrumc_ecs/src/components.rs
@@ -9,7 +9,7 @@ pub trait Component: 'static {}
 
 #[derive(Debug, Component, Constructor)]
 pub struct Position {
-    x: f32,
+    pub(crate) x: f32,
     y: f32,
     z: f32,
 }
@@ -30,7 +30,7 @@ impl Position {
 }
 
 
-pub struct ComponentStorage {
+/*pub struct ComponentStorage {
     storages: HashMap<TypeId, Box<dyn Any>>,
     max_entity_id: usize,
 }
@@ -100,6 +100,73 @@ impl ComponentStorage {
 }
 
 impl ComponentStorage {
+    pub fn query<F: QueryFilter>(&self) -> Query<F> {
+        Query::new(self)
+    }
+}*/
+
+pub struct ComponentStorage {
+    storages: HashMap<TypeId, Box<dyn Any>>,
+    max_entity_id: usize,
+}
+
+impl ComponentStorage {
+    pub fn new() -> Self {
+        ComponentStorage {
+            storages: HashMap::new(),
+            max_entity_id: 0,
+        }
+    }
+
+    pub fn insert<T: Component>(&mut self, entity_id: impl Into<usize>, component: T) {
+        let entity_id = entity_id.into();
+        let storage = self.storages
+            .entry(component_id!(T))
+            .or_insert_with(|| Box::new(SparseSet::<T>::new()));
+
+        let storage = storage.downcast_mut::<SparseSet<T>>().unwrap();
+        storage.insert(entity_id, component);
+        self.max_entity_id = self.max_entity_id.max(entity_id);
+    }
+
+    pub fn get<T: Component>(&self, entity_id: impl Into<usize>) -> Option<&T> {
+        self.storages.get(&component_id!(T))
+            .and_then(|storage| {
+                let storage = storage.downcast_ref::<SparseSet<T>>().unwrap();
+                storage.get(entity_id.into())
+            })
+    }
+
+    pub fn get_mut<T: Component>(&mut self, entity_id: impl Into<usize>) -> Option<&mut T> {
+        self.storages.get_mut(&component_id!(T))
+            .and_then(|storage| {
+                let storage = storage.downcast_mut::<SparseSet<T>>().unwrap();
+                storage.get_mut(entity_id.into())
+            })
+    }
+
+    pub fn remove<T: Component>(&mut self, entity_id: impl Into<usize>) -> Option<T> {
+        self.storages.get_mut(&component_id!(T))
+            .and_then(|storage| {
+                let storage = storage.downcast_mut::<SparseSet<T>>().unwrap();
+                storage.remove(entity_id.into())
+            })
+    }
+
+    pub fn get_storage<T: Component>(&self) -> Option<&SparseSet<T>> {
+        self.storages.get(&component_id!(T))
+            .and_then(|storage| storage.downcast_ref::<SparseSet<T>>())
+    }
+
+    pub fn get_storage_mut<T: Component>(&mut self) -> Option<&mut SparseSet<T>> {
+        self.storages.get_mut(&component_id!(T))
+            .and_then(|storage| storage.downcast_mut::<SparseSet<T>>())
+    }
+
+    pub fn max_entity_id(&self) -> usize {
+        self.max_entity_id
+    }
+
     pub fn query<F: QueryFilter>(&self) -> Query<F> {
         Query::new(self)
     }
--- src/crates/ferrumc_ecs/src/query.rs
+++ src/crates/ferrumc_ecs/src/query.rs
@@ -1,88 +1,146 @@
 use std::marker::PhantomData;
-
 use crate::components::{Component, ComponentStorage};
 
-pub trait QueryFilter: 'static {
+pub trait QueryFilter: 'static  {
     type Item<'a>;
-    fn filter_fetch<'a>(storage: &'a ComponentStorage, entity_id: usize) -> Option<Self::Item<'a>>;
+    unsafe fn filter_fetch<'a>(storage: *const ComponentStorage, entity_id: usize) -> Option<Self::Item<'a>>;
 }
+
+pub trait QueryFilterMut: 'static  {
+    type Item<'a>;
+    unsafe fn filter_fetch_mut<'a>(storage: *mut ComponentStorage, entity_id: usize) -> Option<Self::Item<'a>>;
+}
+
 impl<T: Component> QueryFilter for T {
     type Item<'a> = &'a T;
 
-    fn filter_fetch<'a>(storage: &'a ComponentStorage, entity_id: usize) -> Option<Self::Item<'a>> {
-        storage.get::<T>(entity_id)
+    unsafe fn filter_fetch<'a>(storage: *const ComponentStorage, entity_id: usize) -> Option<Self::Item<'a>> {
+        (*storage).get::<T>(entity_id)
     }
 }
 
+impl<T: Component> QueryFilterMut for T {
+    type Item<'a> = &'a mut T;
+
+    unsafe fn filter_fetch_mut<'a>(storage: *mut ComponentStorage, entity_id: usize) -> Option<Self::Item<'a>> {
+        (*storage).get_mut::<T>(entity_id)
+    }
+}
 
 pub struct Query<'a, F: QueryFilter> {
-    storage: &'a ComponentStorage,
-    _marker: PhantomData<F>,
+    storage: *const ComponentStorage,
+    _marker: PhantomData<&'a F>,
+}
+
+pub struct QueryMut<'a, F: QueryFilterMut> {
+    storage: *mut ComponentStorage,
+    _marker: PhantomData<&'a mut F>,
 }
 
 impl<'a, F: QueryFilter> Query<'a, F> {
     pub fn new(storage: &'a ComponentStorage) -> Self {
         Query {
-            storage,
+            storage: storage as *const ComponentStorage,
             _marker: PhantomData,
         }
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (usize, F::Item<'a>)> + '_ {
-        (0..=self.storage.max_entity_id())
-            .filter_map(|entity_id| {
-                F::filter_fetch(self.storage, entity_id)
-                    .map(|item| (entity_id, item))
-            })
+        let max_entity_id = unsafe { (*self.storage).max_entity_id() };
+        (0..=max_entity_id).filter_map(|entity_id| {
+            unsafe {
+                F::filter_fetch(self.storage, entity_id).map(|item| (entity_id, item))
+            }
+        })
     }
 }
 
-// You can add more implementations for tuples of 3, 4, etc. components if needed
+impl<'a, F: QueryFilterMut> QueryMut<'a, F> {
+    pub fn new(storage: &'a mut ComponentStorage) -> Self {
+        QueryMut {
+            storage: storage as *mut ComponentStorage,
+            _marker: PhantomData,
+        }
+    }
 
-pub trait System {
-    fn run(&mut self, storage: &ComponentStorage);
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = (usize, F::Item<'a>)> + '_ {
+        let max_entity_id = unsafe { (*self.storage).max_entity_id() };
+        (0..=max_entity_id).filter_map(|entity_id| {
+            unsafe {
+                F::filter_fetch_mut(self.storage, entity_id).map(|item| (entity_id, item))
+            }
+        })
+    }
 }
 
-mod tests {
+// Implement for tuples (example with 2 components)
+impl<A: Component, B: Component> QueryFilter for (A, B) {
+    type Item<'a> = (&'a A, &'a B);
+
+    unsafe fn filter_fetch<'a>(storage: *const ComponentStorage, entity_id: usize) -> Option<Self::Item<'a>> {
+        Some((
+            (*storage).get::<A>(entity_id)?,
+            (*storage).get::<B>(entity_id)?
+        ))
+    }
+}
+
+impl<A: Component, B: Component> QueryFilterMut for (A, B) {
+    type Item<'a> = (&'a mut A, &'a mut B);
+
+    unsafe fn filter_fetch_mut<'a>(storage: *mut ComponentStorage, entity_id: usize) -> Option<Self::Item<'a>> {
+        Some((
+            (*storage).get_mut::<A>(entity_id)?,
+            (*storage).get_mut::<B>(entity_id)?
+        ))
+    }
+}
+mod tests  {
     use crate::components::{ComponentStorage, Position, Velocity};
-    use crate::query::System;
+    use crate::query::{Query, QueryMut};
     use crate::world::EntityAllocator;
 
-    pub struct PhysicsSystem;
+    struct MovementSystem;
 
-    impl System for PhysicsSystem {
-        fn run(&mut self, storage: &ComponentStorage) {
-            let query = storage.query::<Position>();
-            for (entity_id, position) in query.iter() {
-                if let Some(velocity) = storage.get::<Velocity>(entity_id) {
-                    println!("Entity {}: Position {:?}, Velocity {:?}", entity_id, position, velocity);
-                }
+    impl MovementSystem {
+        pub fn run(storage: &mut ComponentStorage) {
+            let mut query = QueryMut::<(Position, Velocity)>::new(storage);
+
+            for (_, (pos, vel)) in query.iter_mut() {
+                pos.add_velocity(vel);
+            }
+        }
+    }
+
+    struct EntityLogger;
+
+    impl EntityLogger {
+        pub fn log(storage: &ComponentStorage) {
+            let query = Query::<Position>::new(storage);
+
+            for (entity_id, pos) in query.iter() {
+                println!("Entity {}: {:?}", entity_id, pos);
             }
         }
     }
 
     #[test]
-    fn test_physics_system() {
-        let mut storage = ComponentStorage::new();
+    fn test_movement_system() {
         let mut allocator = EntityAllocator::new();
-
-
-        allocator.allocate(&mut storage)
-            .with(Position::new(0.0, 0.0, 0.0))
-            .with(Velocity::new(1.0, 1.0, 1.0))
-            .build();
+        let mut storage = ComponentStorage::new();
 
         allocator.allocate(&mut storage)
             .with(Position::new(0.0, 0.0, 0.0))
-            .build();
+            .with(Velocity::new(1.0, 1.0, 1.0));
 
         allocator.allocate(&mut storage)
-            .with(Position::new(9.0, 1.0, 5.0))
-            .with(Velocity::new(1.0, 1.0, 1.0))
-            .build();
+            .with(Position::new(1.0, 0.0, 3.0))
+            .with(Velocity::new(2.0, 2.0, 2.0));
 
+        allocator.allocate(&mut storage)
+            .with(Position::new(2.0, 0.0, 6.0));
 
-        let mut system = PhysicsSystem;
-        system.run(&storage);
+        MovementSystem::run(&mut storage);
+        EntityLogger::log(&storage);
     }
 }
