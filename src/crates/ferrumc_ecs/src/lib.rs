9a067e0fc25b630a755731ea3ba69953696f2191
Sweattypalms <stranger8722@gmail.com>
2024-07-21 16:13:40 +0900

    cleanup

--- src/crates/ferrumc_ecs/src/lib.rs
+++ src/crates/ferrumc_ecs/src/lib.rs
@@ -6,30 +6,4 @@ pub mod components;
 pub mod error;
 mod macros;
 pub mod dsa;
-mod query;/*
-#[test]
-fn entry() {
-    main();
-}
-
-fn main() {
-    let mut world = World::new();
-
-    let entity = world.create_entity()
-        .with(Position::new(0.0, 1.0, 0.0))
-        .with(Velocity::new(1.0, 1.0, 1.0))
-        .build();
-
-    // Query example
-    for (entity_id, (pos, vel)) in world.query::<(Position, Velocity)>().iter() {
-        println!("Entity {}: Position {:?}, Velocity {:?}", entity_id, pos, vel);
-    }
-
-    // Mutable query example
-    for (_, (pos, vel)) in world.query_mut::<(Position, Velocity)>().iter_mut() {
-        pos.add_velocity(vel);
-    }
-
-    // Delete entity
-    world.delete_entity(entity).expect("Failed to delete entity");
-}*/
+mod query;
