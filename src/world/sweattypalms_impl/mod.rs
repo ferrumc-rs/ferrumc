3cffb29d6e83d90e8cb4709da17c76bc433130a3
Sweattypalms <stranger8722@gmail.com>
2024-07-27 01:56:52 +0900

    One and multiple regions test

--- src/world/sweattypalms_impl/mod.rs
+++ src/world/sweattypalms_impl/mod.rs
@@ -14,7 +14,7 @@ mod tests {
 
     // NOTE: Will be slower than `release` build. (Approx 10x slower LMAO)
     #[test]
-    fn test_stuff() -> Result<()> {
+    fn test_all_regions() -> Result<()> {
         let start = std::time::Instant::now();
 
         let mut chunks = Vec::<Chunk>::new();
@@ -29,6 +29,20 @@ mod tests {
 
         println!("There are {} chunks in the region file", chunks.len());
         println!("Time taken to read and parse all chunks: {:?}", start.elapsed());
+
+        Ok(())
+    }
+    
+    #[test]
+    fn test_one_chunk() -> Result<()> {
+        let start = std::time::Instant::now();
+        let mut region = get_region("test_region.mca")?;
+        let chunk_data = region.read_chunk(15, 30).expect("Failed to read chunk data").unwrap();
+        let nbt = read(&mut Cursor::new(&chunk_data)).expect("Failed to parse chunk data^1").unwrap();
+        let data = Chunk::from_nbt(&nbt).expect("Failed to parse chunk data^2");
+
+        // println!("{:?}", data);
+        println!("Time taken to read and parse one chunk: {:?}", start.elapsed());
 
         Ok(())
     }
