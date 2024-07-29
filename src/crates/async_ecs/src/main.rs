040caf8f530cdd02c99a49a662ebf869d5609e28
Sweattypalms <stranger8722@gmail.com>
2024-07-30 00:20:13 +0900

    Updated main method to log some temporary values

--- src/crates/async_ecs/src/main.rs
+++ src/crates/async_ecs/src/main.rs
@@ -5,5 +5,18 @@ mod entity;
 fn main() {
     let mut entity_manager = EntityManager::new();
 
-    let _ = entity_manager.create_entity();
+    let entity1 = entity_manager.create_entity();
+    let entity2 = entity_manager.create_entity();
+
+    println!("Entity 1: {:?}", entity1); // Entity { id: 0, generation: 0 }
+    println!("Entity 2: {:?}", entity2); // Entity { id: 1, generation: 0 }
+    
+    // Delete entity 1
+    entity_manager.delete_entity(entity1);
+    
+    // Create a new entity
+    let entity3 = entity_manager.create_entity();
+    
+    // Reuse the ID of the deleted entity
+    println!("Entity 3: {:?}", entity3); // Entity { id: 0, generation: 1 }
 }
