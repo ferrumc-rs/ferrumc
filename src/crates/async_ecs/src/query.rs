248b4ab3c763252bfa49db751acc1384efc39e4b
Sweattypalms <stranger8722@gmail.com>
2024-08-02 19:50:12 +0900

    Updated tests. This is very clean. I love it!

--- src/crates/async_ecs/src/query.rs
+++ src/crates/async_ecs/src/query.rs
@@ -1,6 +1,6 @@
 use std::marker::PhantomData;
 use crate::component::{ComponentRef, ComponentStorage, DynamicComponent};
-use crate::entity::{Entity, EntityManager};
+use crate::entity::{EntityManager};
 
 pub trait QueryItem<'a>: 'a {
     type Item;
@@ -40,7 +40,7 @@ impl<'a, Q: QueryItem<'a>> Query<'a, Q> {
 
     pub fn iter(&self) -> impl Iterator<Item = (usize, Q::Item)> + 'a {
         self.entity_manager.generations.iter().enumerate()
-            .filter_map(|(id, &generation)| {
+            .filter_map(|(id, &_)| {
                 Q::fetch(id, self.component_storage)
                     .map(|item| (id, item))
             })
@@ -72,29 +72,64 @@ impl_query_item_tuple!(A, B, C, D, E);
 
 #[cfg(test)]
 mod tests {
-    use crate::component::{ComponentStorage, Position};
+    use std::sync::OnceLock;
+    use parking_lot::RwLock;
+    use crate::component::{ComponentStorage, Position, Velocity};
     use crate::entity::EntityManager;
     use crate::query::Query;
 
+    /*fn GET_WORLD() -> &(EntityManager, ComponentStorage) {
+        static static_world: OnceLock<(EntityManager, ComponentStorage)> = OnceLock::new();
+
+        static_world.get_or_init(|| {
+            let entity_manager = EntityManager::new();
+            let component_storage = ComponentStorage::new();
+            (entity_manager, component_storage)
+        })
+    }*/
+    type World = (RwLock<EntityManager>, ComponentStorage);
+
+    #[allow(non_snake_case)]
+    fn GET_WORLD() -> &'static World {
+        static STATIC_WORLD: OnceLock<World> = OnceLock::new();
+
+        STATIC_WORLD.get_or_init(|| {
+            let entity_manager = EntityManager::new();
+            let entity_manager = RwLock::new(entity_manager);
+            let component_storage = ComponentStorage::new();
+
+            (entity_manager, component_storage)
+        })
+    }
+
     #[test]
     fn test_basic_query() {
-        let mut em = EntityManager::new();
-        let component_storage = ComponentStorage::new();
+        let (em, component_storage) = GET_WORLD();
+
+        let mut em = em.write();
 
         let entity = em.create_entity();
-        let position = Position { x: 1.5, y: 3.7 };
+        let position = Position { x: 0.0, y: 0.0 };
+        let velocity = Velocity { x: 1.0, y: 5.0 };
         component_storage.insert(entity, position);
+        component_storage.insert(entity, velocity);
 
         let entity2 = em.create_entity();
         let position2 = Position { x: 0f32, y: 1f32 };
         component_storage.insert(entity2, position2);
 
 
+        let query = Query::<(Position, Velocity)>::new(&em, &component_storage);
+
+        query.iter().for_each(|(_, (mut position, velocity))| {
+            position.x += velocity.x;
+            position.y += velocity.y;
+        });
+
         let query = Query::<Position>::new(&em, &component_storage);
 
-        query.iter().for_each(|(id, mut position)| {
-            position.x += 1.0;
-            println!("Entity {} has position ({}, {})", id, position.x, position.y);
+        query.iter().for_each(|(id, position)| {
+            println!("{id}: {:?}", *position)
         });
     }
 
