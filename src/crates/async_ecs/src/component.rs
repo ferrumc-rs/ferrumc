bffea0face144ef5c8abde6ca6dd1e73483b37d4
Sweattypalms <stranger8722@gmail.com>
2024-08-03 02:35:24 +0900

    Fix the async mess

--- src/crates/async_ecs/src/component.rs
+++ src/crates/async_ecs/src/component.rs
@@ -1,9 +1,10 @@
 use std::any::TypeId;
 use std::fmt::Debug;
-use std::future::{IntoFuture};
+use std::future::{Future, IntoFuture};
 use std::ops::{Deref, DerefMut};
-
+use std::sync::Arc;
 use dashmap::DashMap;
+use dashmap::mapref::one::Ref;
 use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 use crate::helpers::sparse_set::SparseSet;
@@ -14,20 +15,20 @@ pub trait DynamicComponent: 'static + Send + Sync + Debug {}
 
 #[derive(Debug)]
 pub struct ComponentRef<'a, T: DynamicComponent> {
-    read_guard: &'a RwLockReadGuard<'a, Box<dyn DynamicComponent>>,
+    read_guard: RwLockReadGuard<'a, Box<dyn DynamicComponent>>,
     _phantom: std::marker::PhantomData<T>,
 }
 
-impl<'a, T: DynamicComponent> Deref for ComponentRef<'a, T> {
+/*impl<'a, T: DynamicComponent> Deref for ComponentRef<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
         unsafe { &*(self.read_guard.as_ref() as *const dyn DynamicComponent as *const T) }
     }
 }
+*/
 
-
-#[derive(Debug)]
+/*#[derive(Debug)]
 pub struct ComponentRefMut<'a, T: DynamicComponent> {
     write_guard: &'a RwLockWriteGuard<'a, Box<dyn DynamicComponent>>,
     _phantom: std::marker::PhantomData<T>,
@@ -38,55 +39,66 @@ impl<'a, T: DynamicComponent> Deref for ComponentRefMut<'a, T> {
     fn deref(&self) -> &Self::Target {
         unsafe { &*(self.write_guard.as_ref() as *const dyn DynamicComponent as *const T) }
     }
-}
+}*/
 
 
+
+
+type StoragesMap = DashMap<TypeId, SparseSet<RwLock<Box<dyn DynamicComponent>>>>;
 
 #[derive(Debug)]
 pub struct ComponentStorage {
-    storages: DashMap<TypeId, SparseSet<RwLock<Box<dyn DynamicComponent>>>>,
+    // storages: DashMap<TypeId, SparseSet<RwLock<Box<dyn DynamicComponent>>>>,s
+    storages: Arc<StoragesMap>,
 }
 
 
 impl ComponentStorage {
     pub fn new() -> Self {
         Self {
-            storages: DashMap::new(),
+            storages: Arc::new(DashMap::new()),
         }
     }
 
     pub fn insert<T: DynamicComponent>(&self, entity_id: impl Into<usize>, component: T) {
         let type_id = TypeId::of::<T>();
 
-        let mut storage = self.storages.entry(type_id).or_insert_with(SparseSet::new);
+        let mut storage = self.storages.entry(type_id).or_insert_with(|| SparseSet::new());
 
         storage.insert(entity_id.into(), RwLock::new(Box::new(component)));
     }
 
-    pub async fn get<T: DynamicComponent>(&self, entity_id: impl Into<usize>) -> Option<ComponentRef<T>> {
+    // pub async fn get<T: DynamicComponent>(&self, entity_id: impl Into<usize>) -> Option<ComponentRef<T>> {
+    pub fn get<T: DynamicComponent>(&self, entity_id: impl Into<usize>) -> Option<impl Future<Output = ComponentRef<T>> + '_> {
         let type_id = TypeId::of::<T>();
-        let storage = self.storages.get(&type_id)?;
         let entity_id = entity_id.into();
 
-        let guard = storage.get(entity_id.into())?;
-        let guard = guard.to_owned();
-        let guard = guard.read().await;
+        let storage = self.storages.get(&type_id)?;
+        let component= storage.get(entity_id)?;
+        
+        Some(async move {
+            let guard = component.read().await;
+            
+            ComponentRef {
+                read_guard: guard,
+                _phantom: std::marker::PhantomData,
+            }
+        })
+        
+        /*let guard = component.read().await;
 
         Some(ComponentRef {
-            // SAFETY: The component cannot possibly outlive the storage itself. Perhaps?
-            // If it does, then it'll be a dangling reference, which is UB (undefined behavior)
-            // Trick to make the borrow checker happy
-            read_guard: unsafe { std::mem::transmute(&guard) },
+            read_guard: guard,
             _phantom: std::marker::PhantomData,
-        })
+        })*/
     }
-    pub async fn get_mut<T: DynamicComponent>(&self, entity_id: impl Into<usize>) -> Option<ComponentRefMut<T>> {
+
+    /*pub async fn get_mut<T: DynamicComponent>(&self, entity_id: impl Into<usize>) -> Option<ComponentRefMut<T>> {
         let type_id = TypeId::of::<T>();
         let storage = self.storages.get(&type_id)?;
         let entity_id = entity_id.into();
 
         let guard = storage.get(entity_id.into())?;
-        let guard = guard.to_owned();
         let guard = guard.write().await;
 
         Some(ComponentRefMut {
@@ -96,7 +108,7 @@ impl ComponentStorage {
             write_guard: unsafe { std::mem::transmute(&guard) },
             _phantom: std::marker::PhantomData,
         })
-    }
+    }*/
 }
 
 
--- src/crates/async_ecs/src/query.rs
+++ src/crates/async_ecs/src/query.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+/*use std::marker::PhantomData;
 use tokio::sync::RwLockWriteGuard;
 use crate::component::{ComponentRef, ComponentRefMut, ComponentStorage, DynamicComponent};
 use crate::entity::EntityManager;
@@ -9,34 +9,45 @@ pub trait QueryItem<'a>: 'a {
     async fn fetch(entity_id: impl Into<usize>, storage: &'a ComponentStorage) -> Option<Self::Item>;
 }
 
-impl<'a, T: DynamicComponent> QueryItem<'a> for T {
+
+impl<'a, T: DynamicComponent> QueryItem<'a> for &'a T {
     type Item = ComponentRef<'a, T>;
 
     async fn fetch(entity_id: impl Into<usize>, storage: &'a ComponentStorage) -> Option<Self::Item> {
         storage.get::<T>(entity_id).await
     }
 }
-pub struct Query<'a, Q, M = ()> {
-    entity_manager: &'a EntityManager,
-    component_storage: &'a ComponentStorage,
-    _marker: PhantomData<(Q, M)>,
-}
-
-// ===== Mutable version of Query =====
-pub struct Mutable;
-pub type QueryMut<'a, Q> = Query<'a, Q, Mutable>;
-
-pub trait QueryItemMut<'a>: 'a {
-    type Item;
-    async fn fetch_mut(entity_id: impl Into<usize>, storage: &'a ComponentStorage) -> Option<Self::Item>;
-}
-
-impl<'a, T: DynamicComponent> QueryItemMut<'a> for T {
+impl<'a, T: DynamicComponent> QueryItem<'a> for &'a mut T {
     type Item = ComponentRefMut<'a, T>;
 
-    async fn fetch_mut(entity_id: impl Into<usize>, storage: &'a ComponentStorage) -> Option<Self::Item> {
+    async fn fetch(entity_id: impl Into<usize>, storage: &'a ComponentStorage) -> Option<Self::Item> {
         storage.get_mut::<T>(entity_id).await
     }
+}
+
+
+
+
+
+// Example implementation of QueryItem for a tuple (A, B)
+
+impl <'a, A: QueryItem<'a>, B: QueryItem<'a>> QueryItem<'a> for (A, B) {
+    type Item = (A::Item, B::Item);
+
+    async fn fetch(entity_id: impl Into<usize>, storage: &'a ComponentStorage) -> Option<Self::Item> {
+        let entity_id: usize = entity_id.into();
+        match (A::fetch(entity_id, storage).await, B::fetch(entity_id, storage).await) {
+            (Some(a), Some(b)) => Some((a, b)),
+            _ => None,
+        }
+    }
+}
+
+
+pub struct Query<'a, Q: QueryItem<'a>> {
+    entity_manager: &'a EntityManager,
+    component_storage: &'a ComponentStorage,
+    _marker: PhantomData<Q>,
 }
 
 impl<'a, Q: QueryItem<'a>> Query<'a, Q> {
@@ -47,6 +58,7 @@ impl<'a, Q: QueryItem<'a>> Query<'a, Q> {
             _marker: PhantomData,
         }
     }
+
 
     pub async fn iter(&self) -> impl Iterator<Item=(usize, Q::Item)> + 'a {
         let entity_manager = self.entity_manager;
@@ -63,120 +75,39 @@ impl<'a, Q: QueryItem<'a>> Query<'a, Q> {
 
         components.into_iter()
     }
-}
 
-impl<'a, Q: QueryItemMut<'a>> Query<'a, Q, Mutable> {
-    pub fn new_mut(entity_manager: &'a EntityManager, component_storage: &'a ComponentStorage) -> Self {
-        Self {
-            entity_manager,
-            component_storage,
-            _marker: PhantomData,
-        }
-    }
-
-    pub async fn iter_mut(&self) -> impl Iterator<Item=(usize, Q::Item)> + 'a {
-        let entity_manager = self.entity_manager;
-        let component_storage = self.component_storage;
-
-        let mut components = vec![];
-
-        for entity_id in 0..entity_manager.len() {
-            let item = Q::fetch_mut(entity_id, component_storage).await;
-            if let Some(item) = item {
-                components.push((entity_id, item));
-            }
-        }
-
-        components.into_iter()
-    }
 }
 
 
-/*macro_rules! impl_query_item_tuple {
-    // ex: impl_query_item_tuple!(A, B, C)
-    ($($ty:ident),*) => {
-        // With lifetimes
-        impl<'a, $($ty: QueryItem<'a>),*> QueryItem<'a> for ($($ty,)*) {
-            type Item = ($($ty::Item,)*);
 
-            async fn fetch(entity_id: impl Into<usize>, storage: &'a ComponentStorage) -> Option<Self::Item> {
-                let entity_id: usize = entity_id.into();
-                Some(($($ty::fetch(entity_id, storage)?,)*))
-            }
-        }
-    };
-}
-
-impl_query_item_tuple!(A);
-impl_query_item_tuple!(A, B);
-impl_query_item_tuple!(A, B, C);
-impl_query_item_tuple!(A, B, C, D);
-impl_query_item_tuple!(A, B, C, D, E);
-*/
-
-#[cfg(test)]
-pub mod tests {
-    use std::sync::OnceLock;
-    use tokio::sync::RwLock;
+mod tests {
     use crate::component::{ComponentStorage, Position, Velocity};
     use crate::entity::EntityManager;
-    use crate::query::Query;
-
-    /*fn GET_WORLD() -> &(EntityManager, ComponentStorage) {
-                static static_world: OnceLock<(EntityManager, ComponentStorage)> = OnceLock::new();
-
-                static_world.get_or_init(|| {
-                    let entity_manager = EntityManager::new();
-                    let component_storage = ComponentStorage::new();
-                    (entity_manager, component_storage)
-                })
-            }*/
-    type World = (RwLock<EntityManager>, ComponentStorage);
-
-    #[allow(non_snake_case)]
-    pub fn GET_WORLD() -> &'static World {
-        static STATIC_WORLD: OnceLock<World> = OnceLock::new();
-
-        STATIC_WORLD.get_or_init(|| {
-            let entity_manager = EntityManager::new();
-            let entity_manager = RwLock::new(entity_manager);
-            let component_storage = ComponentStorage::new();
-
-            (entity_manager, component_storage)
-        })
-    }
 
     #[tokio::test]
-    async fn test_basic_query() {
-        let (em, component_storage) = GET_WORLD();
-        let mut em = em.write().await;
+    async fn test_basic_usage() {
+        let mut  entity_manager = EntityManager::new();
+        let component_storage = ComponentStorage::new();
 
-        let entity = em.create_entity();
+        let entity = entity_manager.create_entity();
         let position = Position { x: 0.0, y: 0.0 };
         let velocity = Velocity { x: 1.0, y: 5.0 };
         component_storage.insert(entity, position);
         component_storage.insert(entity, velocity);
 
-        let entity2 = em.create_entity();
-        let position2 = Position { x: 0f32, y: 1f32 };
-        component_storage.insert(entity2, position2);
-        // QUERY
-        let query = Query::<Position>::new(&em, &component_storage);
+        let entity = entity_manager.create_entity();
+        let position = Position { x: 2.0, y: 3.0 };
+        component_storage.insert(entity, position);
 
-        query.iter().await.for_each(|(id, position)| {
-            println!("{id}: {:?}", *position)
+        let entity = entity_manager.create_entity();
+        let position = Position { x: 4.0, y: 5.0 };
+        let velocity = Velocity { x: 6.0, y: 7.0 };
+        component_storage.insert(entity, position);
+
+        let query = crate::query::Query::<(&mut Position, &Velocity)>::new(&entity_manager, &component_storage);
+
+        query.iter().await.for_each(|(id, (position, velocity))| {
+            println!("{id}: {:?} {:?}", *position, *velocity)
         });
-
-/*
-        query.iter().for_each(|(_, (mut position, velocity))| {
-            position.x += velocity.x;
-            position.y += velocity.y;
-        });
-
-        let query = Query::<Position>::new(&em, &component_storage);
-
-        query.iter().for_each(|(id, position)| {
-            println!("{id}: {:?}", *position)
-        });*/
     }
-}
+}*/
