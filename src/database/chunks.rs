97330928a8dfe7df4189865b4ebedef27f664d53
ReCore-sys <kjshore2004@gmail.com>
2024-09-11 22:18:55 +0930

    Handle error fixed

--- src/database/chunks.rs
+++ src/database/chunks.rs
@@ -1,3 +1,7 @@
+use byteorder::LE;
+use heed::types::{Bytes, U64};
+use heed::Env;
+use tokio::task::spawn_blocking;
 use tracing::{debug, warn};
 
 use crate::database::Database;
@@ -9,44 +13,113 @@ use bincode::config::standard;
 use bincode::{decode_from_slice, encode_to_vec};
 
 impl Database {
+    /// Fetch chunk from database
+    fn get_chunk_from_database(db: &Env, key: &u64) -> Result<Option<Chunk>, Error> {
+        // Initialize read transaction and open chunks table
+        let ro_tx = db.read_txn().unwrap();
+        let database = db
+            .open_database::<U64<LE>, Bytes>(&ro_tx, Some("chunks"))
+            .unwrap()
+            .expect("No table \"chunks\" found. The database should have been initialized");
+
+        // Attempt to fetch chunk from table
+        if let Ok(data) = database.get(&ro_tx, key) {
+            Ok(data.map(|encoded_chunk| {
+                let chunk: (Chunk, usize) = decode_from_slice(encoded_chunk, standard())
+                    .expect("Failed to decode chunk from database");
+                chunk.0
+            }))
+        } else {
+            Err(Error::DatabaseError("Failed to get chunk".into()))
+        }
+    }
+
+    /// Insert a single chunk into database
+    fn insert_chunk_into_database(db: &Env, chunk: &Chunk) -> Result<(), Error> {
+        // Initialize write transaction and open chunks table
+        let mut rw_tx = db.write_txn().unwrap();
+        let database = db
+            .open_database::<U64<LE>, Bytes>(&rw_tx, Some("chunks"))
+            .unwrap()
+            .expect("No table \"chunks\" found. The database should have been initialized");
+
+        // Encode chunk
+        let encoded_chunk = encode_to_vec(chunk, standard()).expect("Failed to encode chunk");
+        let key = hash((chunk.dimension.as_ref().unwrap(), chunk.x_pos, chunk.z_pos));
+
+        // Insert chunk
+        let res = database.put(&mut rw_tx, &key, &encoded_chunk);
+        rw_tx.commit().map_err(|err| {
+            Error::DatabaseError(format!("Unable to commit changes to database: {err}"))
+        })?;
+
+        if let Err(err) = res {
+            Err(Error::DatabaseError(format!(
+                "Failed to insert or update chunk: {err}"
+            )))
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Insert multiple chunks into database
+    /// TODO: Find better name/disambiguation
+    fn insert_chunks_into_database(db: &Env, chunks: &[Chunk]) -> Result<(), Error> {
+        // Initialize write transaction and open chunks table
+        let mut rw_tx = db.write_txn().unwrap();
+        let database = db
+            .open_database::<U64<LE>, Bytes>(&rw_tx, Some("chunks"))
+            .unwrap()
+            .expect("No table \"chunks\" found. The database should have been initialized");
+
+        // Update page
+        for chunk in chunks {
+            // Encode chunk
+            let encoded_chunk = encode_to_vec(chunk, standard()).expect("Failed to encode chunk");
+            let key = hash((chunk.dimension.as_ref().unwrap(), chunk.x_pos, chunk.z_pos));
+
+            // Insert chunk
+            database
+                .put(&mut rw_tx, &key, &encoded_chunk)
+                .map_err(|err| {
+                    Error::DatabaseError(format!("Failed to insert or update chunk: {err}"))
+                })?;
+        }
+
+        // Commit changes
+        rw_tx.commit().map_err(|err| {
+            Error::DatabaseError(format!("Unable to commit changes to database: {err}"))
+        })?;
+        Ok(())
+    }
+
     async fn load_into_cache(&self, key: u64) -> Result<(), Error> {
         let db = self.db.clone();
         let cache = self.cache.clone();
 
         tokio::task::spawn(async move {
-            // This is stupid, but it's the only way to get the lifetime checker to shut up
-            let get_chunk = |db: &rocksdb::DB, key: u64| {
-                let cf = db
-                    .cf_handle("chunks")
-                    .expect("Failed to get column family \"chunks\"");
-                if let Ok(data) = db.get_cf(&cf, key.to_be_bytes()) {
-                    if let Some(encoded) = data {
-                        let chunk: (Chunk, usize) = decode_from_slice(&encoded, standard())
-                            .expect("Failed to decode chunk from database");
-                        Ok(Some(chunk.0))
-                    } else {
-                        Ok(None)
-                    }
-                } else {
-                    Err(Error::DatabaseError("Failed to get chunk".to_string()))
-                }
-            };
-
+            // Check cache
             if cache.contains_key(&key) {
                 debug!("Chunk already exists in cache: {:X}", key);
-            } else {
-                if let Ok(c) = get_chunk(&db, key) {
-                    if c.is_some() {
-                        cache.insert(key, c.unwrap()).await;
-                    } else {
-                        warn!(
-                            "Chunk does not exist in db, can't load into cache: {:X}",
-                            key,
-                        );
-                    }
+            }
+            // If not in cache then search in database
+            else if let Ok(chunk) =
+                spawn_blocking(move || Self::get_chunk_from_database(&db, &key))
+                    .await
+                    .unwrap()
+            {
+                if let Some(chunk) = chunk {
+                    cache.insert(key, chunk).await;
                 } else {
-                    warn!("Error getting chunk: {:X}", key,);
+                    warn!(
+                        "Chunk does not exist in db, can't load into cache: {:X}",
+                        key,
+                    );
                 }
+            }
+            // The chunk don't exist
+            else {
+                warn!("Error getting chunk: {:X}", key,);
             }
         })
         .await?;
@@ -72,29 +145,20 @@ impl Database {
     ///
     /// ```
     pub async fn insert_chunk(&self, value: Chunk) -> Result<(), Error> {
-        let key = hash((value.x_pos, value.z_pos));
-        let result = self.internal_insert_chunk(value.clone()).await;
-        if result.is_ok() {
-            self.cache.insert(key, value).await;
-            Ok(())
-        } else {
-            result
-        }
-    }
-    async fn internal_insert_chunk(&self, value: Chunk) -> Result<(), Error> {
+        // Calculate key of this chunk
+        // WARNING: This key wasn't supposed to include value.dimension in the tuple but it was different from the key used in persistent database most likely a bug.
+        let key = hash((value.dimension.as_ref().unwrap(), value.x_pos, value.z_pos));
+
+        // Insert chunk into persistent database
+        let chunk = value.clone();
         let db = self.db.clone();
-        tokio::task::spawn_blocking(move || {
-            let cf = db
-                .cf_handle("chunks")
-                .expect("Failed to get column family \"chunks\"");
-            let encoded = encode_to_vec(&value, standard()).expect("Failed to encode chunk");
-            let key = hash((value.dimension.unwrap(), value.x_pos, value.z_pos));
-            db.put_cf(&cf, key.to_be_bytes(), encoded)
-                .or(Err(Error::DatabaseError(
-                    "Failed to insert chunk".to_string(),
-                )))
-        })
-        .await?
+        spawn_blocking(move || Self::insert_chunk_into_database(&db, &chunk))
+            .await
+            .unwrap()?;
+
+        // Insert into cache
+        self.cache.insert(key, value).await;
+        Ok(())
     }
 
     /// Get a chunk from the database <br>
@@ -123,40 +187,26 @@ impl Database {
         z: i32,
         dimension: String,
     ) -> Result<Option<Chunk>, Error> {
+        // Calculate key of this chunk and clone database pointer
         let key = hash((dimension, x, z));
+        let db = self.db.clone();
+
+        // First check cache
         if self.cache.contains_key(&key) {
             Ok(self.cache.get(&key).await)
-        } else {
-            if let Ok(chunk) = self.internal_get_chunk(key).await {
-                if chunk.is_some() {
-                    self.cache.insert(key, chunk.clone().unwrap()).await;
-                }
-                Ok(chunk)
-            } else {
-                Ok(None)
-            }
         }
-    }
-
-    async fn internal_get_chunk(&self, key: u64) -> Result<Option<Chunk>, Error> {
-        let db = self.db.clone();
-        tokio::task::spawn_blocking(move || {
-            let cf = db
-                .cf_handle("chunks")
-                .expect("Failed to get column family \"chunks\"");
-            if let Ok(data) = db.get_cf(&cf, key.to_be_bytes()) {
-                if let Some(encoded) = data {
-                    let chunk = decode_from_slice(&encoded, standard())
-                        .expect("Failed to decode chunk from database");
-                    Ok(Some(chunk.0))
-                } else {
-                    Ok(None)
-                }
-            } else {
-                Err(Error::DatabaseError("Failed to get chunk".to_string()))
-            }
-        })
-        .await?
+        // Attempt to get chunk from persistent database
+        else if let Some(chunk) = spawn_blocking(move || Self::get_chunk_from_database(&db, &key))
+            .await
+            .unwrap()?
+        {
+            self.cache.insert(key, chunk.clone()).await;
+            Ok(Some(chunk))
+        }
+        // Chunk do not exist
+        else {
+            Ok(None)
+        }
     }
 
     /// Check if a chunk exists in the database
@@ -178,22 +228,32 @@ impl Database {
     ///
     /// ```
     pub async fn chunk_exists(&self, x: i32, z: i32, dimension: String) -> Result<bool, Error> {
+        // Calculate key and copy database pointer
         let key = hash((dimension, x, z));
+        let db = self.db.clone();
+
+        // Check first cache
         if self.cache.contains_key(&key) {
             Ok(true)
+        // Else check persistent database and load it into cache
         } else {
-            let res = self.internal_chunk_exists(key).await;
-            self.load_into_cache(key).await?;
-            res
-        }
-    }
+            let res = spawn_blocking(move || Self::get_chunk_from_database(&db, &key))
+                .await
+                .unwrap();
 
-    async fn internal_chunk_exists(&self, key: u64) -> Result<bool, Error> {
-        let cf = self
-            .db
-            .cf_handle("chunks")
-            .expect("Failed to get column family \"chunks\"");
-        Ok(self.db.get_cf(&cf, key.to_be_bytes()).is_ok())
+            // WARNING: The previous logic was to order the chunk to be loaded into cache whether it existed or not.
+            // This has been replaced by directly loading the queried chunk into cache
+            match res {
+                Ok(opt) => {
+                    let exist = opt.is_some();
+                    if let Some(chunk) = opt {
+                        self.cache.insert(key, chunk).await;
+                    }
+                    Ok(exist)
+                }
+                Err(err) => Err(err),
+            }
+        }
     }
 
     /// Update a chunk in the database <br>
@@ -215,31 +275,20 @@ impl Database {
     ///
     /// ```
     pub async fn update_chunk(&self, value: Chunk) -> Result<(), Error> {
-        let key = hash((value.x_pos, value.z_pos));
-        let result = self.internal_update_chunk(value.clone()).await;
-        if result.is_ok() {
-            self.cache.insert(key, value).await;
-            Ok(())
-        } else {
-            result
-        }
-    }
+        // Calculate key of this chunk
+        // WARNING: This key wasn't supposed to include value.dimension in the tuple but it was different from the key used in persistent database most likely a bug.
+        let key = hash((value.dimension.as_ref().unwrap(), value.x_pos, value.z_pos));
 
-    async fn internal_update_chunk(&self, value: Chunk) -> Result<(), Error> {
+        // Insert new chunk state into persistent database
+        let chunk = value.clone();
         let db = self.db.clone();
+        spawn_blocking(move || Self::insert_chunk_into_database(&db, &chunk))
+            .await
+            .unwrap()?;
 
-        tokio::task::spawn_blocking(move || {
-            let cf = db
-                .cf_handle("chunks")
-                .expect("Failed to get column family \"chunks\"");
-            let encoded = encode_to_vec(&value, standard()).expect("Failed to encode chunk");
-            let key = hash((value.dimension.unwrap(), value.x_pos, value.z_pos));
-            db.put_cf(&cf, key.to_be_bytes(), encoded)
-                .or(Err(Error::DatabaseError(
-                    "Failed to update chunk".to_string(),
-                )))
-        })
-        .await?
+        // Insert new chunk state into cache
+        self.cache.insert(key, value).await;
+        Ok(())
     }
 
     /// Batch insert chunks into the database <br>
@@ -261,40 +310,28 @@ impl Database {
     ///
     /// ```
     pub async fn batch_insert(&self, values: Vec<Chunk>) -> Result<(), Error> {
-        // TODO: Ewwwww clones, disgusting, fix this
+        // Clone database pointer
+        let db = self.db.clone();
+
+        // Calculate all keys
         let keys = values
             .iter()
             .map(|v| hash((v.dimension.as_ref().unwrap(), v.x_pos, v.z_pos)))
             .collect::<Vec<u64>>();
-        let result = self.internal_batch_insert(values).await;
-        if result.is_ok() {
-            for key in keys {
-                self.load_into_cache(key).await?;
-            }
-            Ok(())
-        } else {
-            result
+
+        // WARNING: The previous logic was to first insert in database and then insert in cache using load_into_cache fn.
+        // This has been modified to avoid having to query database while we already have the data available.
+        // First insert into cache
+        for (key, chunk) in keys.into_iter().zip(&values) {
+            self.cache.insert(key, chunk.clone()).await;
+            self.load_into_cache(key).await?;
         }
-    }
 
-    async fn internal_batch_insert(&self, values: Vec<Chunk>) -> Result<(), Error> {
-        let db = self.db.clone();
-
-        tokio::task::spawn_blocking(move || {
-            let cf = db
-                .cf_handle("chunks")
-                .expect("Failed to get column family \"chunks\"");
-            let mut batch = rocksdb::WriteBatch::default();
-            for value in values {
-                let encoded = encode_to_vec(&value, standard()).expect("Failed to encode chunk");
-                let key = hash((value.dimension.unwrap(), value.x_pos, value.z_pos));
-                batch.put_cf(&cf, key.to_be_bytes(), encoded);
-            }
-            db.write(batch).or(Err(Error::DatabaseError(
-                "Failed to batch insert chunks".to_string(),
-            )))
-        })
-        .await?
+        // Then insert into persistent database
+        spawn_blocking(move || Self::insert_chunks_into_database(&db, &values))
+            .await
+            .unwrap()?;
+        Ok(())
     }
 }
 
