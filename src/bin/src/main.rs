aeb458dc618eab603f02b62538252a98f4410486
Sweattypalms <stranger8722@gmail.com>
2024-09-17 20:24:52 +0900

    ctor creates test file "test.txt"

--- src/bin/src/main.rs
+++ src/bin/src/main.rs
@@ -1,6 +1,20 @@
 // Security
 #![forbid(unsafe_code)]
 
+use std::fs::OpenOptions;
+use std::io::Write;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[ctor::ctor]
+fn foo() {
+    let mut test_file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open("test.txt")
+        .unwrap();
+    writeln!(test_file, "Hello, world!").unwrap();
 }
---
+++ src/lib/events/src/tests/mod.rs
@@ -0,0 +1,4 @@
+#[tokio::test]
+async fn test_something() {
+    println!("bus");
+}
