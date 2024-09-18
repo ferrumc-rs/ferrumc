9b44c891fb7e1919c1961ad9a7c763cb699578e2
nulluser <diuyeung@gmail.com>
2024-09-18 23:36:01 +1000

    Add config logic and stuff very nice.
    TODO: cleanup code.

--- src/utils/config/src/errors.rs
+++ src/utils/config/src/errors.rs
@@ -2,12 +2,24 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum ConfigError {
-    #[error("Something failed lol")]
-    SomeError,
-
     #[error("IO error: {0}")]
     IOError(#[from] std::io::Error),
 
-    #[error("Configuration file TOML read error: {0}")]
-    TomlError(#[from] toml::de::Error),
+    #[error("Configuration file TOML read error:\n{0}")]
+    TomlDeError(#[from] toml::de::Error),
+
+    #[error("Configuration file TOML write error:\n{0}")]
+    TomlSerError(#[from] toml::ser::Error),
+
+    #[error("Failed to read configuration file.")]
+    ConfigLoadError,
+
+    #[error("get_global_config_path was called before set_global_config_path!")]
+    GlobalConfigPathNotSet,
+
+    #[error("Failed to get global_config path.")]
+    SetGlobalConfigPathError,
+
+    #[error("Failed to set configuration file.")]
+    ConfigSetError,
 }
