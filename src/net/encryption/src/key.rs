4680e2bb6a73ba1afabec815c32a9db99069619c
Aiden Orr <aiden.e.orr@gmail.com>
2024-09-17 23:24:58 -0500

    Error handling for RSA

--- src/net/encryption/src/key.rs
+++ src/net/encryption/src/key.rs
@@ -1,39 +1,48 @@
 use rsa::{RsaPublicKey, RsaPrivateKey, pkcs1::EncodeRsaPublicKey, Pkcs1v15Encrypt};
 use rand::rngs::OsRng;
+use crate::errors;
 
 pub struct KeyPair {
-    pub public_key: RsaPublicKey,
-    pub encoded_public_key: der::Document,
-    pub private_key: RsaPrivateKey,
+    pub public_key: RsaPublicKey, //public key side of the keypair (for encryption)
+    pub encoded_public_key: der::Document, //the public key encoded in ASN.1 DER format for publishing to the client
+    pub private_key: RsaPrivateKey, //private key side of the keypair (for decryption)
 }
 
 impl KeyPair {
-    pub fn new() -> Self {
+    pub fn new() -> Result<KeyPair, errors::NetEncryptionError> {
         let mut rng = OsRng;
-        let private_key = RsaPrivateKey::new(&mut rng, 1024).expect("Failed to generate a key");
-        let public_key = RsaPublicKey::from(&private_key);
-        let encoded_public_key = public_key.to_pkcs1_der().expect("Failed to encode public key");
 
-        Self {
+        let private_key = RsaPrivateKey::new(&mut rng, 1024)
+            .map_err(|_e| errors::NetEncryptionError::RsaKeyGenerationError)?;
+
+        let public_key = RsaPublicKey::from(&private_key);
+
+        let encoded_public_key = public_key.to_pkcs1_der()
+            .map_err(|_e| errors::NetEncryptionError::RsaKeyEncodingError)?;
+
+        Ok(Self {
             public_key,
             encoded_public_key,
             private_key,
-        }
+        })
     }
 
-    pub fn encrypt (&self, data: &[u8]) -> Vec<u8> {
+    pub fn encrypt (&self, data: &[u8]) -> Result<Vec<u8>, errors::NetEncryptionError> {
         let mut rng = OsRng;
-        self.public_key.encrypt(&mut rng, Pkcs1v15Encrypt, data).expect("Failed to encrypt data")
+
+        self.public_key.encrypt(&mut rng, Pkcs1v15Encrypt, data)
+            .map_err(|_e| errors::NetEncryptionError::RsaEncryptionError)
     }
 
-    pub fn decrypt (&self, data: &[u8]) -> Vec<u8> {
-        self.private_key.decrypt(Pkcs1v15Encrypt, data).expect("Failed to decrypt data")
+    pub fn decrypt (&self, data: &[u8]) -> Result<Vec<u8>, errors::NetEncryptionError> {
+        self.private_key.decrypt(Pkcs1v15Encrypt, data)
+            .map_err(|_e| errors::NetEncryptionError::RsaDecryptionError)
     }
 }
 
 impl Default for KeyPair {
-    fn default() -> Self {
-        Self::new()
+    fn default() -> KeyPair {
+        Self::new().expect("Keypair generation failed")
     }
 }
 
