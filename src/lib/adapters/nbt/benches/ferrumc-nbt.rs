160ea7d60a4c967929c52309efc20bcbdb11b559
Sweattypalms <stranger8722@gmail.com>
2024-09-26 22:20:15 +0900

    Updated benches to bench Chunk data instead

--- src/lib/adapters/nbt/benches/ferrumc-nbt.rs
+++ src/lib/adapters/nbt/benches/ferrumc-nbt.rs
@@ -1,22 +1,54 @@
 #![feature(portable_simd)]
 
-use std::fs::{OpenOptions, Permissions};
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use fastanvil::Region;
 use fastnbt::Value;
+use ferrumc_macros::NBTDeserialize;
 use nbt as hematite_nbt;
 use std::io::Cursor;
-use fastanvil::Region;
 
 fn bench_ferrumc_nbt(data: &[u8]) {
-    let mut parser = ferrumc_nbt::de::borrow::NbtTape::new(data);
+    /*let mut parser = ferrumc_nbt::de::borrow::NbtTape::new(data);
     parser.parse();
 
-    black_box(parser);
+    black_box(parser);*/
+
+    #[derive(NBTDeserialize)]
+    struct Chunk<'a> {
+        #[nbt(rename = "xPos")]
+        x_pos: i32,
+        #[nbt(rename = "zPos")]
+        z_pos: i32,
+        #[nbt(rename = "Heightmaps")]
+        heightmaps: Heightmaps<'a>
+    }
+
+    #[derive(NBTDeserialize)]
+    struct Heightmaps<'a> {
+        #[nbt(rename = "MOTION_BLOCKING")]
+        motion_blocking: &'a [i64],
+    }
+
+
+    let chunk = Chunk::from_bytes(data).unwrap();
+    assert_eq!(chunk.x_pos, 0);
+    assert_eq!(chunk.z_pos, 32);
+    assert_eq!(chunk.heightmaps.motion_blocking.len(), 37);
 }
 
 fn bench_simdnbt(data: &[u8]) {
-    let nbt = simdnbt::borrow::read(&mut Cursor::new(data)).unwrap();
-    assert!(nbt.is_some());
+    let nbt = simdnbt::borrow::read(&mut std::io::Cursor::new(data)).unwrap();
+
+    let nbt = nbt.unwrap();
+    let nbt = nbt.as_compound();
+    let x_pos = nbt.get("xPos").unwrap().int().unwrap();
+    let z_pos = nbt.get("zPos").unwrap().int().unwrap();
+
+    let motion_blocking = nbt.get("Heightmaps").unwrap().compound().unwrap().get("MOTION_BLOCKING").unwrap().long_array().unwrap();
+
+    assert_eq!(x_pos, 0);
+    assert_eq!(z_pos, 32);
+    assert_eq!(motion_blocking.len(), 37);
 }
 
 fn bench_simdnbt_owned(data: &[u8]) {
@@ -52,22 +84,22 @@ fn hematite_nbt(data: &[u8]) {
 fn criterion_benchmark(c: &mut Criterion) {
     // let cursor = Cursor::new(include_bytes!("../../../../../.etc/benches/region/r.0.0.mca"));
     // let file = std::fs::File::open(r#"D:\Minecraft\framework\ferrumc\ferrumc-2_0\ferrumc\.etc\benches\region\r.0.0.mca"#).unwrap();
-    
-    let file = OpenOptions::new()
-        .write(true)
-        .read(true)
-        .open(r#"D:\Minecraft\framework\ferrumc\ferrumc-2_0\ferrumc\.etc\benches\region\r.0.0.mca"#)
-        .unwrap();
-    
-    
-    let mut region = Region::new(file).unwrap();
-    let chunk = region.iter().next().unwrap().unwrap();
-    let data = chunk.data.as_slice();
+
+    let cursor = include_bytes!("../../../../../.etc/benches/region/r.0.1.mca");
+
+    let buf_reader = std::io::Cursor::new(cursor);
+
+    let mut region = Region::from_stream(buf_reader).unwrap();
+    let next = region.read_chunk(0, 0).unwrap().unwrap();
+
+    // let data = chunk.data.as_slice();
+    let data = next.as_slice();
+
     // let data = include_bytes!("../../../../../.etc/benches/registry_data.nbt");
     let data = ferrumc_nbt::decompress_gzip(data).unwrap();
     let data = data.as_slice();
 
-    let mut group = c.benchmark_group("NBT Parsing");
+    let mut group = c.benchmark_group("Chunk Data NBT Parsing");
     group.throughput(Throughput::Bytes(data.len() as u64));
     group.bench_function("FerrumC NBT", |b| {
         b.iter(|| bench_ferrumc_nbt(black_box(data)))
