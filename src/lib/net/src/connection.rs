972b171c2d7c347816684070fea00483aef7c34d
nulluser0 <diuyueng@gmail.com>
2024-10-13 16:51:22 +1100

    Decode compressed packet done

--- src/lib/net/src/connection.rs
+++ src/lib/net/src/connection.rs
@@ -1,11 +1,11 @@
-use tokio::io::{AsyncWriteExt, BufReader};
+use crate::packets::incoming::PacketSkeleton;
+use crate::{handle_packet, NetResult, ServerState};
+use ferrumc_net_codec::encode::{NetEncode, NetEncodeOpts};
 use std::sync::Arc;
+use tokio::io::{AsyncWriteExt, BufReader};
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use tokio::net::TcpStream;
 use tracing::{trace, warn};
-use ferrumc_net_codec::encode::{NetEncode, NetEncodeOpts};
-use crate::{handle_packet, NetResult, ServerState};
-use crate::packets::incoming::PacketSkeleton;
 
 pub enum ConnectionState {
     Handshaking,
@@ -26,12 +26,14 @@ impl ConnectionState {
 
 pub struct StreamReader {
     pub reader: BufReader<OwnedReadHalf>,
+    pub compressed: bool,
 }
 
 impl StreamReader {
     pub fn new(reader: BufReader<OwnedReadHalf>) -> Self {
         Self {
-            reader
+            reader,
+            compressed: false,
         }
     }
 }
@@ -42,16 +44,18 @@ pub struct StreamWriter {
 
 impl StreamWriter {
     pub fn new(writer: OwnedWriteHalf) -> Self {
-        Self {
-            writer
-        }
+        Self { writer }
     }
 
-    pub async fn send_packet(&mut self, packet: &impl NetEncode, net_encode_opts: &NetEncodeOpts) -> NetResult<()> {
+    pub async fn send_packet(
+        &mut self,
+        packet: &impl NetEncode,
+        net_encode_opts: &NetEncodeOpts,
+    ) -> NetResult<()> {
         let mut buf = Vec::new();
         packet.encode(&mut buf, net_encode_opts)?;
         self.writer.write_all(buf.as_slice()).await?;
-        
+
         Ok(())
     }
 }
@@ -59,31 +63,31 @@ impl StreamWriter {
 pub async fn handle_connection(state: Arc<ServerState>, tcp_stream: TcpStream) -> NetResult<()> {
     let (reader, writer) = tcp_stream.into_split();
 
-    let entity = state.universe
+    let entity = state
+        .universe
         .builder()
         .with(StreamReader::new(BufReader::new(reader)))
         .with(StreamWriter::new(writer))
         .with(ConnectionState::Handshaking)
         .build();
 
-
-    let mut reader = state
-        .universe
-        .get_mut::<StreamReader>(entity)?;
+    let mut reader = state.universe.get_mut::<StreamReader>(entity)?;
 
     'recv: loop {
-        let mut packet_skele = PacketSkeleton::new(&mut reader.reader).await?;
+        let compressed = reader.compressed;
+        let mut packet_skele = PacketSkeleton::new(&mut reader.reader, compressed).await?;
 
         trace!("Received packet: {:?}", packet_skele);
 
         if let Err(e) = handle_packet(
             packet_skele.id,
             entity,
-            &*state.universe
-                .get::<ConnectionState>(entity)?,
+            &*state.universe.get::<ConnectionState>(entity)?,
             &mut packet_skele.data,
             Arc::clone(&state),
-        ).await {
+        )
+        .await
+        {
             warn!("Failed to handle packet: {:?}", e);
             // Kick the player (when implemented).
             break 'recv;
--- src/lib/net/src/packets/incoming/mod.rs
+++ src/lib/net/src/packets/incoming/mod.rs
@@ -1,11 +1,11 @@
-use std::fmt::Debug;
-use crate::NetResult;
+use crate::{errors::NetError, NetResult};
+use ferrumc_config::get_global_config;
+use ferrumc_net_codec::{decode::errors::NetDecodeError, net_types::var_int::VarInt};
 use std::io::Cursor;
+use std::{fmt::Debug, io::Read};
 use tokio::io::{AsyncRead, AsyncReadExt};
-use ferrumc_net_codec::net_types::var_int::VarInt;
 
 pub mod handshake;
-
 
 pub struct PacketSkeleton {
     pub length: usize,
@@ -22,9 +22,16 @@ impl Debug for PacketSkeleton {
     }
 }
 
-
 impl PacketSkeleton {
-    pub async fn new<R: AsyncRead + Unpin>(reader: &mut R) -> NetResult<Self> {
+    pub async fn new<R: AsyncRead + Unpin>(reader: &mut R, compressed: bool) -> NetResult<Self> {
+        match compressed {
+            true => Self::read_compressed(reader).await,
+            false => Self::read_uncompressed(reader).await,
+        }
+    }
+
+    #[inline(always)]
+    async fn read_uncompressed<R: AsyncRead + Unpin>(reader: &mut R) -> NetResult<Self> {
         let length = VarInt::read_async(reader).await?.val as usize;
         let mut buf = {
             let mut buf = vec![0; length];
@@ -37,6 +44,69 @@ impl PacketSkeleton {
 
         Ok(Self {
             length,
+            id: id.val as u8,
+            data: buf,
+        })
+    }
+
+    #[inline(always)]
+    async fn read_compressed<R: AsyncRead + Unpin>(reader: &mut R) -> NetResult<Self> {
+        let packet_length = VarInt::read_async(reader).await?.val as usize;
+        let data_length = VarInt::read_async(reader).await?.val as usize;
+
+        // Uncompressed packet when data length is 0
+        if data_length == 0 {
+            let mut buf = {
+                let mut buf = vec![0; packet_length];
+                reader.read_exact(&mut buf).await?;
+
+                Cursor::new(buf)
+            };
+
+            let id = VarInt::read(&mut buf)?;
+
+            return Ok(Self {
+                length: packet_length,
+                id: id.val as u8,
+                data: buf,
+            });
+        }
+
+        let compression_threshold = get_global_config().network_compression_threshold;
+
+        // https://wiki.vg/Protocol#Packet_format
+        // The Notchian server (but not client) rejects compressed packets smaller than the threshold.
+        // Uncompressed packets exceeding the threshold, however, are accepted.
+        if data_length < compression_threshold as usize {
+            // Compressed packet smaller than threshold
+            // Reject packet
+            return NetResult::Err(NetError::DecoderError(
+                NetDecodeError::CompressedPacketTooSmall(data_length),
+            ));
+        }
+
+        // Here, guaranteed that data_length >= compression_threshold
+        let mut buf = {
+            let mut buf = vec![0; data_length];
+            reader.read_exact(&mut buf).await?;
+
+            Cursor::new(buf)
+        };
+
+        // Decompress data
+        let mut decompressed = Vec::new();
+        {
+            // Scope for decoder
+            let mut decoder = flate2::read::ZlibDecoder::new(&mut buf);
+            decoder.read_to_end(&mut decompressed)?;
+        }
+
+        let mut buf = Cursor::new(decompressed);
+
+        let id = VarInt::read(&mut buf)?;
+
+        Ok(Self {
+            length: packet_length,
             id: id.val as u8,
             data: buf,
         })
