393f4391e5aa6adba0abafa832d748f54b1952ea
Sweattypalms <stranger8722@gmail.com>
2024-10-14 01:07:38 +0900

    No clue

--- src/bin/src/packet_handlers/login_process.rs
+++ src/bin/src/packet_handlers/login_process.rs
@@ -1,8 +1,8 @@
-use tracing::info;
 use ferrumc_macros::event_handler;
-use ferrumc_net::errors::{NetError};
+use ferrumc_net::errors::NetError;
+use ferrumc_net::packets::incoming::login_start::LoginStartEvent;
 use ferrumc_net::GlobalState;
-use ferrumc_net::packets::incoming::login_start::{LoginStartEvent};
+use tracing::info;
 
 #[event_handler]
 async fn handle_login_start(
@@ -10,7 +10,6 @@ async fn handle_login_start(
     _state: GlobalState,
 ) -> Result<LoginStartEvent, NetError> {
     info!("Handling login start event");
-
 
     Ok(login_start_event)
 }
