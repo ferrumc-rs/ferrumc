830aa927b0058590ee7be0ba4b354eecfaa691bf
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-10-16 18:40:33 +0100

    send login success when login start is received

--- src/bin/src/packet_handlers/login_process.rs
+++ src/bin/src/packet_handlers/login_process.rs
@@ -2,14 +2,29 @@ use ferrumc_macros::event_handler;
 use ferrumc_net::errors::NetError;
 use ferrumc_net::packets::incoming::login_start::LoginStartEvent;
 use ferrumc_net::GlobalState;
-use tracing::info;
+use tracing::{info, trace};
+use ferrumc_net::connection::{StreamWriter};
+use ferrumc_net::packets::outgoing::login_success::LoginSuccessPacket;
+use ferrumc_net_codec::encode::NetEncodeOpts;
 
 #[event_handler]
 async fn handle_login_start(
     login_start_event: LoginStartEvent,
     _state: GlobalState,
 ) -> Result<LoginStartEvent, NetError> {
+
     info!("Handling login start event");
 
+    let uuid = login_start_event.login_start_packet.uuid;
+    let username = login_start_event.login_start_packet.username.clone();
+    trace!("Received login start from user with username {}", username);
+
+    //Send a Login Success Response to further the login sequence
+    let response = LoginSuccessPacket::new(uuid, username);
+    let mut writer = _state
+        .universe
+        .get_mut::<StreamWriter>(login_start_event.conn_id)?;
+
+    writer.send_packet(&response, &NetEncodeOpts::WithLength).await?;
     Ok(login_start_event)
 }
