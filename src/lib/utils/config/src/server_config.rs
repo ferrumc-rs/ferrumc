3d258c59960c383a10ddf7cffa100a114176492a
nulluser0 <diuyueng@gmail.com>
2024-10-19 20:19:28 +1100

    more error handling

--- src/lib/utils/config/src/server_config.rs
+++ src/lib/utils/config/src/server_config.rs
@@ -4,9 +4,9 @@
 
 use crate::errors::ConfigError;
 use crate::statics::{get_global_config, set_global_config};
+use ferrumc_general_purpose::paths::get_root_path;
 use serde_derive::{Deserialize, Serialize};
 use tracing::{error, info};
-use ferrumc_general_purpose::paths::get_root_path;
 
 /// The server configuration struct.
 ///
@@ -130,10 +130,16 @@ impl ServerConfig {
             Err(e) => {
                 // Check if we can prompt the user to create a new configuration file.
                 if !prompt_user {
-                    return Err(ConfigError::ConfigLoadError);
+                    return Err(ConfigError::ConfigLoadError(
+                        path.to_string_lossy().to_string(),
+                    ));
                 }
                 // Config could not be read. Prompt the user to create a new one from ServerConfig::Default.
-                error!("Could not read configuration file: {}", e);
+                error!(
+                    "Could not read configuration file \"{}\" : {}",
+                    path.to_string_lossy().to_string(),
+                    e
+                );
                 error!("Would you like to create a new config file? (y/N): ");
 
                 let user_input = {
@@ -148,18 +154,23 @@ impl ServerConfig {
 
                     DEFAULT_CONFIG
                 } else {
-                    return Err(ConfigError::ConfigLoadError);
+                    return Err(ConfigError::ConfigLoadError(
+                        path.to_string_lossy().to_string(),
+                    ));
                 }
             }
         };
-
 
         let config: ServerConfig = match toml::from_str(config) {
             Ok(config) => config,
             Err(e) => {
                 // Config could not be serialized. Prompt the user to create
                 // a new one from ServerConfig::Default.
-                error!("Could not read configuration file: {}", e);
+                error!(
+                    "Could not read configuration file \"{}\" : {}",
+                    path.to_string_lossy().to_string(),
+                    e
+                );
                 error!("Would you like to create a new config file? Your old configuration will be saved as \"config.toml.bak\". (y/N): ");
                 let mut input = String::new();
                 std::io::stdin().read_line(&mut input)?;
@@ -174,7 +185,9 @@ impl ServerConfig {
                     info!("Configuration file created.");
                 } else {
                     // User did not enter "y". Return the error.
-                    return Err(ConfigError::ConfigLoadError);
+                    return Err(ConfigError::ConfigLoadError(
+                        path.to_string_lossy().to_string(),
+                    ));
                 }
                 // Deserialize the configuration file into a ServerConfig struct.
                 toml::from_str(DEFAULT_CONFIG)?
