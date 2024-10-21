68bfaeb02a47450c260e65e8f70c2abcd07c60e4
ReCore-sys <kjshore2004@gmail.com>
2024-10-21 18:42:43 +1030

    World setup kinda done

--- src/lib/world/src/errors.rs
+++ src/lib/world/src/errors.rs
@@ -1,7 +1,31 @@
+use std::io::ErrorKind;
 use thiserror::Error;
+use crate::errors::WorldError::{GenericIOError, PermissionError};
 
 #[derive(Debug, Clone, Error)]
 pub enum WorldError {
-    #[error("Something failed lol")]
-    SomeError,
+    #[error("Invalid World Path: {0}")]
+    InvalidWorldPath(String),
+    #[error("Invalid Backend: {0}")]
+    InvalidBackend(String),
+    #[error("Invalid Compressor: {0}")]
+    InvalidCompressor(String),
+    #[error("Invalid Cache Size: {0}")]
+    InvalidCacheSize(String),
+    #[error("Invalid Import Path: {0}")]
+    InvalidImportPath(String),
+    #[error("Unable to obtain permission to access file/folder: {0}")]
+    PermissionError(String),
+    #[error("Some kind of IO error occurred: {0}")]
+    GenericIOError(String),
+}
+
+impl From<std::io::Error> for WorldError {
+    fn from(err: std::io::Error) -> Self {
+        match err.kind() {
+            ErrorKind::PermissionDenied => PermissionError(err.to_string()),
+            ErrorKind::ReadOnlyFilesystem => PermissionError(err.to_string()),
+            _ => GenericIOError(err.to_string()),
+        }
+    }
 }
