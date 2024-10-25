2eb9c55e2725b0b8448d014adf0c88d48f2c1ded
AnonymousBit <68566858+AnonymousBit0111@users.noreply.github.com>
2024-10-25 19:20:41 +0300

    added back start_ticking

--- src/bin/src/main.rs
+++ src/bin/src/main.rs
@@ -1,8 +1,10 @@
 // Security or something like that
 #![forbid(unsafe_code)]
 
-use std::sync::{atomic::AtomicBool, Arc};
-use tracing::{error, info};
+use std::{sync::{atomic::AtomicBool, Arc}, time::Duration};
+use ferrumc_events::infrastructure::Event;
+use tokio::time::Instant;
+use tracing::{debug, error, info};
 use ferrumc_ecs::Universe;
 use ferrumc_net::{packets::outgoing::tick_event::TickEvent, ServerState};
 
@@ -25,12 +27,37 @@ async fn main() {
     }
 }
 
+async fn start_ticking(net_state: Arc<ServerState>) {
+    // is game time saved in a file??
+    let mut tick = 0;
+    loop {
+        let required_end = Instant::now() + Duration::from_millis(50);
+        // TODO handle error
+        let res = TickEvent::trigger(TickEvent::new(tick), net_state.clone()).await;
+
+        if res.is_err() {
+            debug!("error : {:?}", res);
+        }
+        let now = Instant::now();
+        if required_end > now {
+            tokio::time::sleep(required_end - now).await;
+
+        } else {
+            let time_debt = now - required_end;
+            info!("running behind! by : {}ms", time_debt.as_millis());
+        }
+
+        tick += 1;
+    }
+}
+
 async fn entry() -> Result<()> {
     let listener = ferrumc_net::server::create_server_listener().await?;
 
-    let state = ServerState::new(Universe::new());
+    let state = Arc::new(ServerState::new(Universe::new()));
 
-    ferrumc_net::server::listen(Arc::new(state), listener).await?;
+    tokio::task::spawn(start_ticking(state.clone()));
+    ferrumc_net::server::listen(state, listener).await?;
 
     Ok(())
 }
