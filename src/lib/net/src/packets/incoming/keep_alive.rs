ed01a609e19f5388b14b127f0a202e9006f945b3
AnonymousBit <68566858+AnonymousBit0111@users.noreply.github.com>
2024-11-01 08:25:11 +0300

    changed info! to debug! and a TODO

--- src/lib/net/src/packets/incoming/keep_alive.rs
+++ src/lib/net/src/packets/incoming/keep_alive.rs
@@ -3,7 +3,7 @@ use crate::packets::IncomingPacket;
 use crate::{NetResult, ServerState};
 use ferrumc_macros::{packet, NetDecode};
 use std::sync::Arc;
-use tracing::info;
+use tracing::debug;
 
 #[derive(NetDecode)]
 #[packet(packet_id = 0x18, state = "play")]
@@ -15,10 +15,11 @@ impl IncomingPacket for IncomingKeepAlivePacket {
     async fn handle(self, conn_id: usize, state: Arc<ServerState>) -> NetResult<()> {
         let mut last_keep_alive = state.universe.get_mut::<KeepAlive>(conn_id)?;
         if self.id != last_keep_alive.id {
-            info!(
+            debug!(
                 "Invalid keep alive packet received from {:?} with id {:?} (expected {:?})",
                 conn_id, self.id, last_keep_alive.id
             );
+            // TODO Kick player
         } else {
             *last_keep_alive = KeepAlive::from(self.id);
         }
