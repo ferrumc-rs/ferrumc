36b267d58b3e04b4208f91022d75c927dcb68742
Oliver <oliverswater@gmail.com>
2024-12-10 21:21:38 +0000

    add serialise deserialise and partialeq traits

--- src/lib/core/src/identity/player_identity.rs
+++ src/lib/core/src/identity/player_identity.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct PlayerIdentity {
     pub username: String,
     pub uuid: u128,
