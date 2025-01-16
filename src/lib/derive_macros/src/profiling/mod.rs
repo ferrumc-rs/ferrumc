8570c0385b2e8cf690ceb0df20db1e15a545d289
Sweattypalms <stranger8722@gmail.com>
2025-01-16 14:54:09 +0900

    cargo fmt and fixed macro expansion for profiler

--- src/lib/derive_macros/src/profiling/mod.rs
+++ src/lib/derive_macros/src/profiling/mod.rs
@@ -3,10 +3,11 @@ use quote::{quote, ToTokens};
 
 #[allow(unused_variables)]
 pub fn profile_fn(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item: proc_macro2::TokenStream = item.into();
     let name = format!("profiler/{}", attr.to_string().replace("\"", "")).to_token_stream();
     let res = quote! {
-        #[tracing::instrument(name = $name)]
-        $item
+        #[tracing::instrument(name = #name)]
+        #item
     };
 
     res.into()
