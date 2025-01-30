78b8b31988e70ddf5d1c0831224e784d102027b1
ReCore <kjshore2004@gmail.com>
2025-01-30 13:34:06 +1030

    Acknowledge block breaking

--- src/lib/net/src/packets/incoming/player_action.rs
+++ src/lib/net/src/packets/incoming/player_action.rs
@@ -1,6 +1,10 @@
+use crate::connection::StreamWriter;
+use crate::packets::outgoing::block_change_ack::BlockChangeAck;
 use crate::packets::IncomingPacket;
 use crate::NetResult;
+use ferrumc_core::chunks::chunk_receiver::ChunkReceiver;
 use ferrumc_macros::{packet, NetDecode};
+use ferrumc_net_codec::encode::NetEncodeOpts;
 use ferrumc_net_codec::net_types::network_position::NetworkPosition;
 use ferrumc_net_codec::net_types::var_int::VarInt;
 use ferrumc_state::ServerState;
@@ -17,7 +21,7 @@ pub struct PlayerAction {
 }
 
 impl IncomingPacket for PlayerAction {
-    async fn handle(self, _: usize, state: Arc<ServerState>) -> NetResult<()> {
+    async fn handle(self, conn_id: usize, state: Arc<ServerState>) -> NetResult<()> {
         // https://minecraft.wiki/w/Minecraft_Wiki:Projects/wiki.vg_merge/Protocol?oldid=2773393#Player_Action
         match self.status.val {
             0 => {
@@ -34,6 +38,28 @@ impl IncomingPacket for PlayerAction {
                         },
                     )
                     .await?;
+                {
+                    let packet = BlockChangeAck {
+                        sequence: self.sequence,
+                    };
+                    if let Ok(mut conn) = state.universe.get_mut::<&mut StreamWriter>(conn_id) {
+                        conn.send_packet(packet, &NetEncodeOpts::WithLength)?;
+                    } else {
+                        debug!(
+                            "Player disconnected before we could send the BlockChangeAck packet"
+                        );
+                    }
+                }
+                {
+                    let q = state.universe.query::<&mut ChunkReceiver>();
+                    for (_, mut chunk_receiver) in q {
+                        chunk_receiver.queue_chunk_resend(
+                            self.location.x >> 4,
+                            self.location.z >> 4,
+                            "overworld".to_string(),
+                        );
+                    }
+                }
             }
             1 => {
                 debug!("You shouldn't be seeing this in creative mode.");
