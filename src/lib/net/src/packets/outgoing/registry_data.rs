ad71fb37edafb97fedddb36f8febe3d1151454df
ReCore <kjshore2004@gmail.com>
2025-06-04 03:34:59 +0930

    Registry stuff, just need to fight borrow checker

--- src/lib/net/src/packets/outgoing/registry_data.rs
+++ src/lib/net/src/packets/outgoing/registry_data.rs
@@ -1,7 +1,8 @@
 use ferrumc_macros::{packet, NetEncode};
-use ferrumc_nbt::{NBTSerializeOptions, NbtTape};
+use ferrumc_nbt::NbtTape;
 use ferrumc_net_codec::net_types::length_prefixed_vec::LengthPrefixedVec;
-use std::io::Write;
+use std::io::{Read, Write};
+use tracing::debug;
 
 #[derive(NetEncode)]
 #[packet(packet_id = "registry_data", state = "configuration")]
@@ -28,45 +29,95 @@ pub struct RegistryEntry<'a> {
 impl RegistryDataPacket<'_> {
     // TODO: bake this. and make it return just the bytes instead.
     pub fn get_registry_packets() -> Vec<Self> {
-        let registry_nbt_buf = include_bytes!("../../../../../../assets/data/registry.nbt");
-
-        let mut tape = NbtTape::new(registry_nbt_buf);
-        tape.parse();
-        let mut serializer_machine = NbtTape::new(registry_nbt_buf);
-        serializer_machine.parse();
-
-        let root = tape.root.as_ref().map(|(_, b)| b).unwrap();
-        let root = root.as_compound().unwrap();
-
         let mut packets = vec![];
 
-        /*let (name, element) = &root[1];*/
-        for (name, element) in root {
-            // TOP LEVEL
-            let element = element.as_compound().unwrap();
+        let dir = std::env::current_dir()
+            .unwrap()
+            .join(r#"..\..\..\assets\data\registries"#);
 
+        let mut registry_nbt_buf = vec![].as_slice();
+
+        for file_name in dir.iter() {
+            let file_name = file_name.to_str().unwrap().split('/').next_back().unwrap();
+            let registry_name = ("minecraft:".to_string()
+                + &*file_name
+                .clone()
+                .strip_suffix(".nbt")
+                .unwrap_or(file_name)
+                .replace("worldgen", "worldgen/biome"));
+            debug!("Opening registry file: {file_name}");
+            registry_nbt_buf = std::fs::read(file_name).unwrap().as_slice();
+            if registry_nbt_buf.is_empty() {
+                continue; // Skip empty files
+            }
             let mut entries = vec![];
-            for (name, element) in element {
+
+            let mut tape = NbtTape::new(registry_nbt_buf);
+
+            tape.parse();
+
+            if tape.root.is_none() {
+                debug!("No root found in registry file: {file_name}");
+                continue; // Skip files without a root
+            }
+            let tape = tape.root.as_ref().unwrap().1.as_compound().unwrap();
+
+            let mut serializer_machine = NbtTape::new(registry_nbt_buf);
+            serializer_machine.parse();
+
+            for (name, element) in tape {
                 let mut data = vec![];
                 element
                     .serialize_as_network(
                         &mut serializer_machine,
                         &mut data,
-                        &NBTSerializeOptions::Network,
+                        &ferrumc_nbt::NBTSerializeOptions::Network,
                     )
                     .unwrap_or_else(|_| panic!("Failed to serialize entry for {name}"));
 
-                entries.push(RegistryEntry {
+                let entry = RegistryEntry {
                     id: name,
-                    data: if data.is_empty() {
-                        None
-                    } else {
-                        Some(data)
-                    },
-                });
+                    data: if data.is_empty() { None } else { Some(data) },
+                };
+                entries.push(entry);
             }
-            packets.push(RegistryDataPacket::new(name, entries));
+            packets.push(RegistryDataPacket::new(
+                registry_name.clone().as_str(),
+                entries,
+            ));
         }
+
+        // let mut tape = NbtTape::new(registry_nbt_buf);
+        // tape.parse();
+        // let mut serializer_machine = NbtTape::new(registry_nbt_buf);
+        // serializer_machine.parse();
+        //
+        // let root = tape.root.as_ref().map(|(_, b)| b).unwrap();
+        // let root = root.as_compound().unwrap();
+        //
+        // /*let (name, element) = &root[1];*/
+        // for (name, element) in root {
+        //     // TOP LEVEL
+        //     let element = element.as_compound().unwrap();
+        //
+        //     let mut entries = vec![];
+        //     for (name, element) in element {
+        //         let mut data = vec![];
+        //         element
+        //             .serialize_as_network(
+        //                 &mut serializer_machine,
+        //                 &mut data,
+        //                 &NBTSerializeOptions::Network,
+        //             )
+        //             .unwrap_or_else(|_| panic!("Failed to serialize entry for {name}"));
+        //
+        //         entries.push(RegistryEntry {
+        //             id: name,
+        //             data: if data.is_empty() { None } else { Some(data) },
+        //         });
+        //     }
+        //     packets.push(RegistryDataPacket::new(name, entries));
+        // }
 
         packets
     }
@@ -81,6 +132,7 @@ mod tests {
     use super::RegistryDataPacket;
     use ferrumc_net_codec::encode::{NetEncode, NetEncodeOpts};
     use serde_json::Value;
+    use std::collections::HashMap;
     use std::io::{Cursor, Write};
 
     #[test]
@@ -89,23 +141,21 @@ mod tests {
         let json_file = include_bytes!("../../../../../../.etc/registry.json");
         let val: HashMap<String, Value> = serde_json::from_slice(json_file).unwrap();
 
-        let mut nbt = Cursor::new(Vec::<u8>::new());
-
-
-        craftflow_nbt::to_writer(&mut nbt, &val).unwrap();
-        let nbt = nbt.into_inner();
-        println!("cwd {}", std::env::current_dir().unwrap().display());
-        let dump_path = std::env::current_dir()
-            .unwrap()
-            .join(r#"..\..\..\assets\data\"#);
-        println!("dump path {}", dump_path.display());
-        let open_file = std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .truncate(true)
-            .open(dump_path.join("registry.nbt"));
-        //
-        open_file.unwrap().write_all(nbt.as_slice()).unwrap();
+        for (key, value) in val {
+            let key = key
+                .replace("minecraft:", "")
+                .replace("worldgen/biome", "worldgen");
+            let mut nbt = Cursor::new(Vec::<u8>::new());
+            craftflow_nbt::to_writer(&mut nbt, &value).unwrap();
+            let nbt = nbt.into_inner();
+            let dump_path = std::env::current_dir()
+                .unwrap()
+                .join(r#"..\..\..\assets\data\registries"#)
+                .join(format!("{key}.nbt"));
+            println!("dump path {}", dump_path.display());
+            let mut open_file = std::fs::File::create(dump_path).unwrap();
+            open_file.write_all(nbt.as_slice()).unwrap();
+        }
     }
 
     #[test]
