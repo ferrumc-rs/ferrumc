6a64cceb1c363f18db98be9bdab418f273d0cd02
ReCore <kjshore2004@gmail.com>
2025-06-11 13:26:04 +0930

    Networking now gracefully closes

--- src/bin/src/game_loop.rs
+++ src/bin/src/game_loop.rs
@@ -3,6 +3,7 @@ use crate::packet_handlers::{play_packets, register_player_systems};
 use crate::register_events::register_events;
 use crate::register_resources::register_resources;
 use crate::systems::register_game_systems;
+use crate::systems::shutdown_systems::register_shutdown_systems;
 use bevy_ecs::prelude::World;
 use bevy_ecs::schedule::ExecutorKind;
 use crossbeam_channel::Sender;
@@ -20,15 +21,26 @@ use tracing::{debug, error, info, info_span, trace, warn, Instrument};
 const NS_PER_SECOND: u64 = 1_000_000_000;
 
 pub fn start_game_loop(global_state: GlobalState) -> Result<(), BinaryError> {
-    // Setup channels and stuff for new connections
+    // Setup the ECS world and schedules
     let mut ecs_world = World::new();
-    let sender_struct = Arc::new(ferrumc_net::create_packet_senders(&mut ecs_world));
-    let (new_conn_send, new_conn_recv) = crossbeam_channel::unbounded();
-
-    let global_state_res = GlobalStateResource(global_state.clone());
 
     let mut schedule = bevy_ecs::schedule::Schedule::default();
     schedule.set_executor_kind(ExecutorKind::MultiThreaded);
+
+    // This schedule is ticked once when the server is shutting down
+    // If you need to run any cleanup systems, add them to `ferrumc::systems::shutdown_systems::register_shutdown_systems`
+    let mut shutdown_schedule = bevy_ecs::schedule::Schedule::default();
+
+    // Setup channels and stuff for new connections
+    let sender_struct = Arc::new(ferrumc_net::create_packet_senders(&mut ecs_world));
+    let (new_conn_send, new_conn_recv) = crossbeam_channel::unbounded();
+
+    // Setup shutdown related channels
+    let (shutdown_send, shutdown_recv) = tokio::sync::oneshot::channel();
+    let (shutdown_response_send, shutdown_response_recv) = crossbeam_channel::unbounded();
+
+    // Register systems and resources
+    let global_state_res = GlobalStateResource(global_state.clone());
 
     register_events(&mut ecs_world);
     register_resources(&mut ecs_world, new_conn_recv, global_state_res);
@@ -36,12 +48,17 @@ pub fn start_game_loop(global_state: GlobalState) -> Result<(), BinaryError> {
     register_player_systems(&mut schedule);
     register_game_systems(&mut schedule);
 
+    register_shutdown_systems(&mut shutdown_schedule);
+
     let ns_per_tick = Duration::from_nanos(NS_PER_SECOND / get_global_config().tps as u64);
 
     // Start the TCP connection acceptor
-    tcp_conn_acceptor(global_state.clone(), sender_struct, Arc::new(new_conn_send))?;
+    tcp_conn_acceptor(global_state.clone(), sender_struct, Arc::new(new_conn_send), shutdown_recv, shutdown_response_send)?;
 
-    info!("Server is ready in {}", format_duration(Instant::now().duration_since(*global_state.start_time.clone())));
+    info!(
+        "Server is ready in {}",
+        format_duration(Instant::now().duration_since(*global_state.start_time.clone()))
+    );
 
     while !global_state
         .shut_down
@@ -75,6 +92,16 @@ pub fn start_game_loop(global_state: GlobalState) -> Result<(), BinaryError> {
         }
     }
 
+    shutdown_schedule.run(&mut ecs_world);
+
+    // tell the TCP connection acceptor to shut down
+    trace!("Sending shutdown signal to TCP connection acceptor");
+    shutdown_send.send(()).expect("Failed to send shutdown signal");
+
+    // Wait until the TCP connection acceptor has shut down
+    trace!("Waiting for TCP connection acceptor to shut down");
+    shutdown_response_recv.recv().expect("Failed to receive shutdown response");
+
     Ok(())
 }
 
@@ -83,6 +110,8 @@ fn tcp_conn_acceptor(
     state: GlobalState,
     packet_sender: Arc<PacketSender>,
     sender: Arc<Sender<NewConnection>>,
+    mut shutdown_notify: tokio::sync::oneshot::Receiver<()>,
+    shutdown_response: Sender<()>,
 ) -> Result<(), BinaryError> {
     let named_thread = std::thread::Builder::new().name("TokioNetworkThread".to_string());
     named_thread.spawn(move || {
@@ -101,28 +130,41 @@ fn tcp_conn_acceptor(
                         ));
                     };
                     while !state.shut_down.load(std::sync::atomic::Ordering::Relaxed) {
-                        let (stream, _) = listener
-                            .accept()
-                            .await
-                            .expect("Failed to accept TCP connection");
-                        let addy = stream.peer_addr()?;
-                        debug!("Got TCP connection from {}", addy);
-                        tokio::spawn({
-                            let state = Arc::clone(&state);
-                            let packet_sender = Arc::clone(&packet_sender);
-                            let sender = Arc::clone(&sender);
-                            async move {
-                                _ = handle_connection(state, stream, packet_sender, sender)
-                                    .instrument(info_span!("conn", %addy).or_current())
-                                    .await;
+                        // Wait for a new connection or shutdown signal
+                        tokio::select! {
+                            accept_result = listener.accept() => {
+                                match accept_result {
+                                    Ok((stream, _)) => {
+                                        let addy = stream.peer_addr()?;
+                                        debug!("Got TCP connection from {}", addy);
+                                        tokio::spawn({
+                                            let state = Arc::clone(&state);
+                                            let packet_sender = Arc::clone(&packet_sender);
+                                            let sender = Arc::clone(&sender);
+                                            async move {
+                                                _ = handle_connection(state, stream, packet_sender, sender)
+                                                    .instrument(info_span!("conn", %addy).or_current())
+                                                    .await;
+                                            }
+                                        });
+                                    }
+                                    Err(e) => {
+                                        error!("Failed to accept TCP connection: {:?}", e);
+                                    }
+                                }
                             }
-                        });
+                            _ = &mut shutdown_notify => {
+                                trace!("Shutdown signal received on notify channel");
+                                break;
+                            }
+                        }
                     }
-                    debug!("Shutting down TCP connection acceptor thread");
                     Ok(())
                 }
             })?;
-            info!("Shutting down TCP connection acceptor");
+            trace!("Shutting down TCP connection acceptor");
+
+            shutdown_response.send(()).expect("Failed to send shutdown response");
             Ok::<(), BinaryError>(())
         }));
         if let Err(e) = caught_panic {
