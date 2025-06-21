ed1c8db9c311f86a8be8a4580631c4d4d63bc83c
Outspending <Dxrippy15@gmail.com>
2025-06-21 17:00:18 -0600

    Revert "PDC Integration"
    This reverts commit 650a3c3de654db153b03da86f4f13ddcce24c772.

--- src/lib/pdc/src/load.rs
+++
@@ -1,9 +0,0 @@
-use crate::{container::PersistentDataContainer, errors::PersistentDataError};
-
-pub struct PersistentContainerLoader;
-
-impl PersistentContainerLoader {
-    pub fn load_from_file(path: &str) -> Result<PersistentDataContainer, PersistentDataError> {
-        todo!()
-    }
-}
