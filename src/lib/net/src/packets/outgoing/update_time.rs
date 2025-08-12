ba5102c3c64ec0898945d167981acc2327868704
Outspending <Dxrippy15@gmail.com>
2025-08-12 11:56:14 -0600

    Refactor day/night cycle and UpdateTimePacket
    Refactored the day_night_cycle_system to improve time_of_day increment logic and broadcasting efficiency. UpdateTimePacket now derives Copy and Clone for easier packet reuse.

--- src/lib/net/src/packets/outgoing/update_time.rs
+++ src/lib/net/src/packets/outgoing/update_time.rs
@@ -1,7 +1,7 @@
 use ferrumc_macros::{packet, NetEncode};
 use std::io::Write;
 
-#[derive(NetEncode)]
+#[derive(NetEncode, Copy, Clone)]
 #[packet(packet_id = "set_time", state = "play")]
 pub struct UpdateTimePacket {
     pub world_age: i64,
