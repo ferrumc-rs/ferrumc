a7043b624d522779b112511f18e124b94c2eff1a
Martin <66840021+Mcrtin@users.noreply.github.com>
2025-09-15 14:45:31 +0200

    fix spline

--- src/lib/world_gen/src/overworld/spline.rs
+++ src/lib/world_gen/src/overworld/spline.rs
@@ -12,35 +12,48 @@ impl CubicSpline {
         let x = match self.spline_type {
             SplineType::RidgesFolded => ridges_folded,
             SplineType::Ridges => ridges,
-            SplineType::Continents => erosion,
-            SplineType::Erosion => continents,
+            SplineType::Continents => continents,
+            SplineType::Erosion => erosion,
         };
         let n = self.points.len();
         assert!(n > 0);
+        let i = match self.points.iter().rposition(|v| v.x <= x) {
+            Some(idx) => idx,
+            None => {
+                // x is before the first point → extend linearly from 0
+                return Self::linear_extend(
+                    x,
+                    &self.points[0],
+                    continents,
+                    erosion,
+                    ridges_folded,
+                    ridges,
+                );
+            }
+        };
 
-        // find i such that xs[i] <= x < xs[i+1]
-        let Some((i, point)) = self.points.iter().enumerate().find(|(_, p)| p.x <= x) else {
+        if i == self.points.len() - 1 {
+            // x is after the last point → extend from last
             return Self::linear_extend(
                 x,
-                &self.points[n - 1],
+                &self.points[i],
                 continents,
                 erosion,
                 ridges_folded,
                 ridges,
             );
-        };
-        if i == n - 1 {
-            return Self::linear_extend(x, point, continents, erosion, ridges_folded, ridges);
         }
+        let point = &self.points[i];
 
         let point2 = &self.points[i + 1];
         let y0 = point.y.get(continents, erosion, ridges_folded, ridges);
         let y1 = point2.y.get(continents, erosion, ridges_folded, ridges);
-        let t = (x - point.x) / (point2.x - point.x);
-
         let dx = point2.x - point.x;
-        let h1 = point.slope * dx - (y1 - y0);
-        let h2 = -point2.slope * dx + (y1 - y0);
+        let t = (x - point.x) / dx;
+
+        let dy = y1 - y0;
+        let h1 = point.slope * dx - dy;
+        let h2 = -point2.slope * dx + dy;
 
         y0.lerp(y1, t) + t * (1.0 - t) * h1.lerp(h2, t)
     }
@@ -57,7 +70,7 @@ impl CubicSpline {
     }
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub enum SplineValue {
     Const(f32),
     Spline(CubicSpline),
@@ -74,7 +87,7 @@ impl SplineValue {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 pub enum SplineType {
     RidgesFolded,
     Ridges,
@@ -82,7 +95,7 @@ pub enum SplineType {
     Erosion,
 }
 /// One knot of the spline
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct SplinePoint {
     pub x: f32,
     pub y: SplineValue,
@@ -106,8 +119,34 @@ impl SplinePoint {
     }
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct CubicSpline {
     spline_type: SplineType,
     pub points: Vec<SplinePoint>, //TODO: const maybe
+}
+
+#[test]
+fn test_spline() {
+    let cubic3 = CubicSpline::new(
+        SplineType::Continents,
+        vec![
+            SplinePoint::constant(-1.1, 0.044, 0.0),
+            SplinePoint::constant(-1.02, -0.2222, 0.0),
+            SplinePoint::constant(-0.51, -0.2222, 0.0),
+            SplinePoint::constant(-0.44, -0.12, 0.0),
+            SplinePoint::constant(-0.18, -0.12, 0.0),
+        ],
+    );
+    let spline = CubicSpline::new(
+        SplineType::Continents,
+        vec![
+            SplinePoint::constant(-1.1, 0.044, 0.0),
+            SplinePoint::constant(-1.02, -0.2222, 0.0),
+            SplinePoint::constant(-0.51, -0.2222, 0.0),
+            SplinePoint::constant(-0.44, -0.12, 0.0),
+            SplinePoint::constant(-0.18, -0.12, 0.0),
+            SplinePoint::spline(1.0, cubic3),
+        ],
+    );
+    assert_eq!(spline.sample(0.0, 0.0, 0.0, 0.0), -0.12)
 }
