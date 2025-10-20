e566534a58662ad5bb115058a2ace27963d0f59f
Sunnickel <office@alexander-reiter.at>
2025-10-20 09:54:46 +0200

    removed useless allocations and bug that would've removed only the first player in the queue

--- src/bin/src/systems/connection_killer.rs
+++ src/bin/src/systems/connection_killer.rs
@@ -11,59 +11,55 @@ pub fn connection_killer(
     mut cmd: Commands,
     state: Res<GlobalStateResource>,
 ) {
-    let mut disconnecting_player: Option<(Entity, &StreamWriter, &PlayerIdentity)> = None;
     while let Some((disconnecting_entity, reason)) = state.0.players.disconnection_queue.pop() {
-        if query.contains(disconnecting_entity) && disconnecting_player.is_none() {
-            let entity_result = query.get(disconnecting_entity);
-
-            match entity_result {
-                Ok(player_result) => {
-                    disconnecting_player = Some(player_result);
-                }
-                Err(e) => {
-                    disconnecting_player = None;
-                    trace!("Disconnecting player not in query: {}", e);
-                }
-            }
-        }
-
-        for (entity, conn, player_identity) in query.iter() {
-            if disconnecting_entity == entity {
-                info!(
-                    "Player {} ({}) disconnected: {}",
-                    player_identity.username,
-                    player_identity.uuid,
-                    reason.as_deref().unwrap_or("No reason")
-                );
-                if conn.running.load(std::sync::atomic::Ordering::Relaxed) {
-                    trace!(
-                        "Sending disconnect packet to player {}",
-                        player_identity.username
-                    );
-                    if let Err(e) = conn.send_packet_ref(
-                        &ferrumc_net::packets::outgoing::disconnect::DisconnectPacket {
-                            reason: TextComponent::from(
-                                reason.as_deref().unwrap_or("Disconnected"),
-                            ),
-                        },
-                    ) {
-                        warn!(
-                            "Failed to send disconnect packet to player {}: {:?}",
-                            player_identity.username, e
+        let entity_result = query.get(disconnecting_entity);
+        match entity_result {
+            Ok(disconnecting_player) => {
+                for (entity, conn, player_identity) in query.iter() {
+                    if disconnecting_entity == entity {
+                        info!(
+                            "Player {} ({}) disconnected: {}",
+                            player_identity.username,
+                            player_identity.uuid,
+                            reason.as_deref().unwrap_or("No reason")
                         );
-                    }
-                } else {
-                    trace!(
+                        if conn.running.load(std::sync::atomic::Ordering::Relaxed) {
+                            trace!(
+                                "Sending disconnect packet to player {}",
+                                player_identity.username
+                            );
+                            if let Err(e) = conn.send_packet_ref(
+                                &ferrumc_net::packets::outgoing::disconnect::DisconnectPacket {
+                                    reason: TextComponent::from(
+                                        reason.as_deref().unwrap_or("Disconnected"),
+                                    ),
+                                },
+                            ) {
+                                warn!(
+                                    "Failed to send disconnect packet to player {}: {:?}",
+                                    player_identity.username, e
+                                );
+                            }
+                        } else {
+                            trace!(
                         "Connection for player {} is not running, skipping disconnect packet",
                         player_identity.username
                     );
-                }
-            } else {
-                if disconnecting_player.is_some() {
-                    system_messages::player_leave::handle(disconnecting_player.unwrap().2, entity);
+                        }
+                    } else {
+                        if disconnecting_player.is_some() {
+                            system_messages::player_leave::handle(
+                                disconnecting_player.unwrap().2,
+                                entity,
+                            );
+                        }
+                    }
+                    cmd.entity(entity).despawn();
                 }
             }
-            cmd.entity(entity).despawn();
+            Err(e) => {
+                warn!("Player's entity has already been removed: {}", e);
+            }
         }
     }
 }
