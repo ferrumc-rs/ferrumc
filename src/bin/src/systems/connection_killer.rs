dcd51485b263a607fdc29633d7ed173ffc1fe540
unknown <queentimer12@gmail.com>
2025-10-23 08:38:49 -0300

    chore: added player disconnect event in connection killer

--- src/bin/src/systems/connection_killer.rs
+++ src/bin/src/systems/connection_killer.rs
@@ -1,21 +1,36 @@
 use crate::systems::system_messages;
-use bevy_ecs::prelude::{Commands, Entity, Query, Res};
-use ferrumc_core::identity::player_identity::PlayerIdentity;
+use bevy_ecs::{
+    event::EventWriter,
+    prelude::{Entity, Query, Res},
+};
+use ferrumc_core::{
+    conn::player_disconnect_event::PlayerDisconnectEvent,
+    data::player::PlayerData,
+    identity::player_identity::PlayerIdentity,
+    transform::{grounded::OnGround, position::Position, rotation::Rotation},
+};
 use ferrumc_net::connection::StreamWriter;
 use ferrumc_state::GlobalStateResource;
 use ferrumc_text::TextComponent;
 use tracing::{info, trace, warn};
 
 pub fn connection_killer(
-    query: Query<(Entity, &StreamWriter, &PlayerIdentity)>,
-    mut cmd: Commands,
+    query: Query<(
+        Entity,
+        &StreamWriter,
+        &PlayerIdentity,
+        &Position,
+        &OnGround,
+        &Rotation,
+    )>,
+    mut dispatch_events: EventWriter<PlayerDisconnectEvent>,
     state: Res<GlobalStateResource>,
 ) {
     while let Some((disconnecting_entity, reason)) = state.0.players.disconnection_queue.pop() {
         let entity_result = query.get(disconnecting_entity);
         match entity_result {
             Ok(disconnecting_player) => {
-                for (entity, conn, player_identity) in query.iter() {
+                for (entity, conn, player_identity, position, on_ground, rotation) in query.iter() {
                     if disconnecting_entity == entity {
                         info!(
                             "Player {} ({}) disconnected: {}",
@@ -49,7 +64,12 @@ pub fn connection_killer(
                     } else {
                         system_messages::player_leave::handle(disconnecting_player.2, entity);
                     }
-                    cmd.entity(entity).despawn();
+                    let player_disconnect = PlayerDisconnectEvent {
+                        data: PlayerData::new(position, on_ground.0, "overworld", rotation),
+                        identity: player_identity.to_owned(),
+                        entity,
+                    };
+                    dispatch_events.write(player_disconnect);
                 }
             }
             Err(e) => {
