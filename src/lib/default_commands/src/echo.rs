b96d38792e1ce16be9aa13beb2abc0b918487101
Nils Krüger <radsteve@radsteve.net>
2025-11-03 16:10:24 +0100

    fix: allow mutable bevy args for commands and improve error message for world bevy args

--- src/lib/default_commands/src/echo.rs
+++ src/lib/default_commands/src/echo.rs
@@ -1,50 +1,15 @@
-use std::time::Duration;
 
 use bevy_ecs::prelude::*;
 use ferrumc_commands::{
-    arg::{
-        primitive::{string::QuotableString, PrimitiveArgument},
-        CommandArgument, ParserResult,
-    },
-    CommandContext, Sender, Suggestion,
+    Sender, arg::primitive::string::GreedyString
 };
 use ferrumc_core::identity::player_identity::PlayerIdentity;
 use ferrumc_macros::command;
 use ferrumc_text::{TextComponent, TextComponentBuilder};
 
-struct TestArg(String);
-
-impl CommandArgument for TestArg {
-    fn parse(ctx: &mut CommandContext) -> ParserResult<Self> {
-        Ok(Self(ctx.input.read_string()))
-    }
-
-    fn primitive() -> PrimitiveArgument {
-        PrimitiveArgument::word()
-    }
-
-    fn suggest(ctx: &mut CommandContext) -> Vec<Suggestion> {
-        ctx.input.read_string();
-
-        vec![
-            Suggestion::of("egg"),
-            Suggestion::of("cheese"),
-            Suggestion::of("fish"),
-        ]
-    }
-}
-
-#[command("love")]
-fn love(#[sender] sender: Sender, #[arg] arg: TestArg, #[arg] duration: Duration) {
-    sender.send_message(
-        TextComponent::from(format!("i've loved {} for {:?}", arg.0, duration)),
-        false,
-    );
-}
-
 #[command("echo")]
 fn test_command(
-    #[arg] message: QuotableString,
+    #[arg] message: GreedyString,
     #[sender] sender: Sender,
     query: Query<&PlayerIdentity>,
 ) {
