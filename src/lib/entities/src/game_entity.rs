f9f3c8cef1cdc0f6b2f51ee9f5b4ed6c7546c17b
Tonguechaude <evan.challias@tonguechaude.fr>
2025-11-04 18:50:02 +0100

    fix : add TypeName trait bound

--- src/lib/entities/src/game_entity.rs
+++ src/lib/entities/src/game_entity.rs
@@ -1,9 +1,10 @@
 use bevy_ecs::prelude::Commands;
 use ferrumc_state::GlobalState;
+use typename::TypeName;
 
 /// Trait for game entities that have behavior/AI
 /// Each entity type can implement tick() to update its state
-pub trait GameEntity: Send + Sync + 'static {
+pub trait GameEntity: Send + Sync + TypeName + 'static {
     fn tick(&mut self, _state: &GlobalState, _commands: &mut Commands) {
         // Default: no-op for stateless entities
     }
