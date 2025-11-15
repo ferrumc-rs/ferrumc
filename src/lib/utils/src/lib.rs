4c006829aee86fc268cc6c4776f73b2498e787a8
Dylan Rogers <opendylan@proton.me>
2025-11-15 14:13:32 +0000

    add back root! macro because I realise we kind of need it...

--- src/lib/utils/src/lib.rs
+++ src/lib/utils/src/lib.rs
@@ -1,2 +1,54 @@
 pub mod errors;
 pub mod formatting;
+
+/// Gets the fully qualified path to the root of the project and joins it with the given path.
+///
+/// # Arguments
+///
+/// * `from_root` - The path to join with the root of the project
+///
+/// # Returns
+///
+/// The fully qualified path to the root of the project joined with the given path
+///
+/// # Example
+///
+/// ```
+/// use std::path::Path;
+/// use ferrumc_utils::root;
+///
+/// assert!(Path::new(&root!("Cargo.toml")).exists());
+/// ```
+#[macro_export]
+macro_rules! root {
+    ($from_root:literal) => {{
+        let delimiter = if cfg!(windows) { "\\" } else { "/" };
+        let root = std::path::absolute(file!())
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string();
+        let root = root
+            .split(delimiter)
+            .take_while(|&x| x != "src")
+            .collect::<Vec<&str>>()
+            .join(delimiter);
+        let path_from_root = std::path::Path::new($from_root);
+        std::path::absolute(root)
+            .unwrap()
+            .join(path_from_root)
+            .to_str()
+            .unwrap()
+            .to_string()
+    }};
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    #[test]
+    fn test_root() {
+        assert!(Path::new(&root!("Cargo.toml")).exists());
+    }
+}
