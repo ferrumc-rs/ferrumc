a3dc27b1ef653fc8fa061117db9aa8ba81176f1d
MSKatKing <mskatking08@gmail.com>
2025-11-23 04:06:29 -0600

    Create NBT<T> wrapper trait for NetEncode

--- src/lib/adapters/nbt/src/lib.rs
+++ src/lib/adapters/nbt/src/lib.rs
@@ -5,7 +5,31 @@ pub mod ser;
 
 pub type Result<T> = std::result::Result<T, NBTError>;
 
+use std::io::Write;
+use tokio::io::AsyncWrite;
 pub use de::borrow::{NbtTape, NbtTapeElement};
 pub use de::converter::FromNbt;
 pub use errors::NBTError;
+use ferrumc_net_codec::encode::{NetEncode, NetEncodeOpts};
+use ferrumc_net_codec::encode::errors::NetEncodeError;
 pub use ser::{NBTSerializable, NBTSerializeOptions};
+
+pub struct NBT<T> {
+    inner: T,
+}
+
+impl<T> NBT<T> {
+    pub fn new(inner: T) -> Self {
+        Self { inner }
+    }
+}
+
+impl<T: NBTSerializable> NetEncode for NBT<T> {
+    fn encode<W: Write>(&self, writer: &mut W, _opts: &NetEncodeOpts) -> std::result::Result<(), NetEncodeError> {
+        Ok(self.inner.serialize(writer, &NBTSerializeOptions::Network))
+    }
+
+    async fn encode_async<W: AsyncWrite + Unpin>(&self, writer: &mut W, _opts: &NetEncodeOpts) -> std::result::Result<(), NetEncodeError> {
+        Ok(self.inner.serialize_async(writer, &NBTSerializeOptions::Network).await)
+    }
+}
