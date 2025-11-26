5c2544ac659077e1669b0a1e08b06c259d907c2b
Tonguechaude <evan.challias@tonguechaude.fr>
2025-11-26 21:00:42 +0100

    fix : rename spawn_pig to entity_spawn

--- src/lib/messages/src/lib.rs
+++ src/lib/messages/src/lib.rs
@@ -19,5 +19,5 @@ pub use player_leave::*;
 pub mod change_gamemode;
 pub use change_gamemode::*;
 
-pub mod spawn_pig;
-pub use spawn_pig::SpawnEntityEvent;
+pub mod entity_spawn;
+pub use entity_spawn::SpawnEntityEvent;
