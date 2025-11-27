3e6efdac9008109f310f84feddc012e3a5bda7b8
MSKatKing <mskatking08@gmail.com>
2025-11-27 09:43:25 -0600

    Change plugin message struct to have a generic type for data instead of Vec<u8>

--- src/lib/net/src/packets/outgoing/client_bound_plugin_message.rs
+++ src/lib/net/src/packets/outgoing/client_bound_plugin_message.rs
@@ -1,23 +1,33 @@
 use ferrumc_macros::{packet, NetEncode};
-use ferrumc_net_codec::encode::{NetEncode, NetEncodeOpts};
+use ferrumc_net_codec::encode::NetEncode;
 
 const SERVER_VERSION: &str = concat!("FerrumC v", env!("FERRUMC_VERSION"), env!("BUILD_TYPE"));
 
+type BrandChannelData = String;
+
 #[derive(NetEncode, Clone)]
 #[packet(packet_id = "custom_payload", state = "configuration")]
-pub struct ClientBoundPluginMessagePacket {
+pub struct ClientBoundPluginMessagePacket<T: NetEncode> {
     pub channel: String,
-    pub data: Vec<u8>,
+    pub data: T,
 }
 
-impl ClientBoundPluginMessagePacket {
-    pub fn brand() -> ClientBoundPluginMessagePacket {
-        let mut data = Vec::<u8>::new();
-        SERVER_VERSION
-            .encode(&mut data, &NetEncodeOpts::None)
-            .expect("failed to write brand name to buffer");
+impl ClientBoundPluginMessagePacket<BrandChannelData> {
+    pub fn brand() -> ClientBoundPluginMessagePacket<BrandChannelData> {
         Self {
             channel: "minecraft:brand".to_string(),
+            data: SERVER_VERSION.to_string(),
+        }
+    }
+}
+
+impl<T: NetEncode> ClientBoundPluginMessagePacket<T> {
+    pub fn with_data_channel(
+        channel: impl Into<String>,
+        data: T,
+    ) -> ClientBoundPluginMessagePacket<T> {
+        Self {
+            channel: channel.into(),
             data,
         }
     }
