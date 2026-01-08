f1a924fb0a4d94e7de0f53b786d2547cf0f7103c
MSKatKing <mskatking08@gmail.com>
2026-01-07 19:08:05 -0600

    Fix errors from merge

--- src/lib/world/src/benches/chunk.rs
+++ src/lib/world/src/benches/chunk.rs
@@ -1,5 +1,6 @@
 use criterion::Criterion;
-use ferrumc_world::chunk::{Chunk, ChunkBlockPos};
+use ferrumc_world::chunk::Chunk;
+use ferrumc_world::pos::ChunkBlockPos;
 
 pub fn bench_chunks(c: &mut Criterion) {
     let mut chunk = Chunk::new_empty();
@@ -7,7 +8,7 @@ pub fn bench_chunks(c: &mut Criterion) {
     for x in 0..16 {
         for z in 0..16 {
             for y in -64..256 {
-                chunk.set_block(ChunkBlockPos { x, y, z }, 130);
+                chunk.set_block(ChunkBlockPos::new(x, y, z), 130);
             }
         }
     }
@@ -19,7 +20,7 @@ pub fn bench_chunks(c: &mut Criterion) {
             for x in 0..16 {
                 for z in 0..16 {
                     for y in -64..256 {
-                        chunk.set_block(ChunkBlockPos { x, y, z }, 130);
+                        chunk.set_block(ChunkBlockPos::new(x, y, z), 130);
                     }
                 }
             }
@@ -31,7 +32,7 @@ pub fn bench_chunks(c: &mut Criterion) {
             for x in 0..16 {
                 for z in 0..16 {
                     for y in -64..256 {
-                        let a = chunk.get_block(ChunkBlockPos { x, y, z });
+                        let a = chunk.get_block(ChunkBlockPos::new(x, y, z));
                         assert_eq!(a, 130)
                     }
                 }
--- src/lib/world/src/benches/world.rs
+++ src/lib/world/src/benches/world.rs
@@ -1,4 +1,3 @@
-mod cache;
 mod chunk;
 mod edit_bench;
 
