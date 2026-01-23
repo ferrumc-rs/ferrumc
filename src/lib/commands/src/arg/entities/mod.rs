970e1bfe740b4e8462cce83da21577ba1cb2db24
ReCore <kjshore2004@gmail.com>
2026-01-23 16:32:06 +1030

    Entity selector tests

--- src/lib/commands/src/arg/entities/mod.rs
+++ src/lib/commands/src/arg/entities/mod.rs
@@ -7,10 +7,10 @@ mod random_player;
 use crate::arg::primitive::PrimitiveArgument;
 use crate::arg::{CommandArgument, ParserResult};
 use crate::{CommandContext, Suggestion};
-use ::uuid::Uuid;
 use bevy_ecs::prelude::Entity;
 use ferrumc_core::identity::entity_identity::EntityIdentity;
 use ferrumc_core::identity::player_identity::PlayerIdentity;
+use ::uuid::Uuid;
 
 /// Represents an entity argument in a command.
 /// It can be a player name, UUID, or special selectors like @e, @p, @r, @a.
@@ -78,7 +78,7 @@ impl CommandArgument for EntityArgument {
                 suggestions.push(Suggestion {
                     content: name.clone(),
                     tooltip: Some(ferrumc_nbt::NBT::new(
-                        uuid::Uuid::from_u128(*uuid)
+                        Uuid::from_u128(*uuid)
                             .as_hyphenated()
                             .to_string()
                             .to_uppercase()
@@ -112,5 +112,227 @@ impl EntityArgument {
                 .map(|e| vec![e])
                 .unwrap_or_default(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{Command, CommandInput, Sender};
+    use bevy_ecs::prelude::World;
+    use ferrumc_core::identity::entity_identity::EntityIdentity;
+    use ferrumc_core::identity::player_identity::PlayerIdentity;
+    use std::sync::Arc;
+
+    #[test]
+    fn test_parse_entity_argument() {
+        let mut ctx = CommandContext {
+            input: CommandInput {
+                input: "Steve".to_string(),
+                cursor: 0,
+            },
+            command: Arc::new(Command {
+                name: "",
+                args: vec![],
+            }),
+            sender: Sender::Server,
+        };
+        let arg = EntityArgument::parse(&mut ctx).unwrap();
+        assert_eq!(arg, EntityArgument::PlayerName("Steve".to_string()));
+
+        let mut ctx = CommandContext {
+            input: CommandInput {
+                input: "@e".to_string(),
+                cursor: 0,
+            },
+            command: Arc::new(Command {
+                name: "",
+                args: vec![],
+            }),
+            sender: Sender::Server,
+        };
+        let arg = EntityArgument::parse(&mut ctx).unwrap();
+        assert_eq!(arg, EntityArgument::AnyEntity);
+
+        let mut ctx = CommandContext {
+            input: CommandInput {
+                input: "@p".to_string(),
+                cursor: 0,
+            },
+            command: Arc::new(Command {
+                name: "",
+                args: vec![],
+            }),
+            sender: Sender::Server,
+        };
+        let arg = EntityArgument::parse(&mut ctx).unwrap();
+        assert_eq!(arg, EntityArgument::NearestPlayer);
+
+        let mut ctx = CommandContext {
+            input: CommandInput {
+                input: "@r".to_string(),
+                cursor: 0,
+            },
+            command: Arc::new(Command {
+                name: "",
+                args: vec![],
+            }),
+            sender: Sender::Server,
+        };
+        let arg = EntityArgument::parse(&mut ctx).unwrap();
+        assert_eq!(arg, EntityArgument::RandomPlayer);
+
+        let mut ctx = CommandContext {
+            input: CommandInput {
+                input: "@a".to_string(),
+                cursor: 0,
+            },
+            command: Arc::new(Command {
+                name: "",
+                args: vec![],
+            }),
+            sender: Sender::Server,
+        };
+        let arg = EntityArgument::parse(&mut ctx).unwrap();
+        assert_eq!(arg, EntityArgument::AnyPlayer);
+
+        let uuid_str = "123e4567-e89b-12d3-a456-426614174000";
+        let mut ctx = CommandContext {
+            input: CommandInput {
+                input: uuid_str.to_string(),
+                cursor: 0,
+            },
+            command: Arc::new(Command {
+                name: "",
+                args: vec![],
+            }),
+            sender: Sender::Server,
+        };
+        let arg = EntityArgument::parse(&mut ctx).unwrap();
+        assert_eq!(
+            arg,
+            EntityArgument::Uuid(Uuid::parse_str(uuid_str).unwrap())
+        );
+    }
+
+    #[test]
+    fn test_resolves_name() {
+        let mut world = World::new();
+        let entity = world
+            .spawn((
+                EntityIdentity {
+                    entity_id: 0,
+                    uuid: Uuid::new_v4(),
+                },
+                PlayerIdentity {
+                    username: "Steve".to_string(),
+                    uuid: Default::default(),
+                    short_uuid: 0,
+                    properties: vec![],
+                },
+            ))
+            .id();
+
+        let arg = EntityArgument::PlayerName("Steve".to_string());
+        let result = arg.resolve(
+            world
+                .query::<(Entity, Option<&EntityIdentity>, Option<&PlayerIdentity>)>()
+                .iter(&world),
+        );
+        assert_eq!(result, vec![entity]);
+    }
+
+    #[test]
+    fn test_resolves_uuid() {
+        let mut world = World::new();
+        let test_uuid = Uuid::new_v4();
+        let entity = world
+            .spawn((EntityIdentity {
+                entity_id: 0,
+                uuid: test_uuid,
+            },))
+            .id();
+        let arg = EntityArgument::Uuid(test_uuid);
+        let result = arg.resolve(
+            world
+                .query::<(Entity, Option<&EntityIdentity>, Option<&PlayerIdentity>)>()
+                .iter(&world),
+        );
+        assert_eq!(result, vec![entity]);
+    }
+
+    #[test]
+    fn test_resolves_any_entity() {
+        let mut world = World::new();
+        let entity1 = world
+            .spawn(EntityIdentity {
+                entity_id: 0,
+                uuid: Uuid::new_v4(),
+            })
+            .id();
+        let entity2 = world
+            .spawn(EntityIdentity {
+                entity_id: 1,
+                uuid: Uuid::new_v4(),
+            })
+            .id();
+        let arg = EntityArgument::AnyEntity;
+        let result = arg.resolve(
+            world
+                .query::<(Entity, Option<&EntityIdentity>, Option<&PlayerIdentity>)>()
+                .iter(&world),
+        );
+        assert_eq!(result.len(), 2);
+        assert!(result.contains(&entity1));
+        assert!(result.contains(&entity2));
+    }
+
+    #[test]
+    fn test_resolves_any_player() {
+        let mut world = World::new();
+        let entity1 = world
+            .spawn((
+                EntityIdentity {
+                    entity_id: 0,
+                    uuid: Uuid::new_v4(),
+                },
+                PlayerIdentity {
+                    username: "Steve".to_string(),
+                    uuid: Uuid::new_v4(),
+                    short_uuid: 0,
+                    properties: vec![],
+                },
+            ))
+            .id();
+        let entity2 = world
+            .spawn((
+                EntityIdentity {
+                    entity_id: 1,
+                    uuid: Uuid::new_v4(),
+                },
+                PlayerIdentity {
+                    username: "Alex".to_string(),
+                    uuid: Uuid::new_v4(),
+                    short_uuid: 1,
+                    properties: vec![],
+                },
+            ))
+            .id();
+        let non_player_entity = world
+            .spawn(EntityIdentity {
+                entity_id: 2,
+                uuid: Uuid::new_v4(),
+            })
+            .id();
+        let arg = EntityArgument::AnyPlayer;
+        let result = arg.resolve(
+            world
+                .query::<(Entity, Option<&EntityIdentity>, Option<&PlayerIdentity>)>()
+                .iter(&world),
+        );
+        assert_eq!(result.len(), 2);
+        assert!(result.contains(&entity1));
+        assert!(result.contains(&entity2));
+        assert!(!result.contains(&non_player_entity));
     }
 }
