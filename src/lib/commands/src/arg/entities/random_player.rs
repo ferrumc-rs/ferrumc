e143be58b9a11da30b0ee7096bb96287e660a913
ReCore <kjshore2004@gmail.com>
2026-01-26 17:13:44 +1030

    Fix compile error

--- src/lib/commands/src/arg/entities/random_player.rs
+++ src/lib/commands/src/arg/entities/random_player.rs
@@ -6,7 +6,7 @@ use rand::prelude::IteratorRandom;
 pub(crate) fn resolve_random_player(
     iter: impl Iterator<Item = (Entity, Option<&EntityIdentity>, Option<&PlayerIdentity>)>,
 ) -> Option<Entity> {
-    let mut rng = rand::rng();
+    let mut rng = rand::thread_rng();
     iter.filter_map(|(entity, _, player_id)| {
         if player_id.is_some() {
             Some(entity)
