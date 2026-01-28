a0def5ee676935dcd3206fa4e1f1fd8c23965554
Tonguechaude <evan.challias@tonguechaude.fr>
2026-01-28 22:45:03 +0100

    feat : add neutral entities

--- src/lib/entities/src/bundles/neutral/mod.rs
+++ src/lib/entities/src/bundles/neutral/mod.rs
@@ -1,21 +1,37 @@
 // Neutral entity bundles - entities that attack only when provoked
 pub mod bee;
+pub mod cave_spider;
 pub mod dolphin;
+pub mod drowned;
+pub mod enderman;
 pub mod fox;
 pub mod goat;
+pub mod iron_golem;
 pub mod llama;
 pub mod panda;
+pub mod piglin;
 pub mod polar_bear;
 pub mod pufferfish;
+pub mod spider;
+pub mod trader_llama;
 pub mod wolf;
+pub mod zombified_piglin;
 
 // Re-exports
 pub use bee::BeeBundle;
+pub use cave_spider::CaveSpiderBundle;
 pub use dolphin::DolphinBundle;
+pub use drowned::DrownedBundle;
+pub use enderman::EndermanBundle;
 pub use fox::FoxBundle;
 pub use goat::GoatBundle;
+pub use iron_golem::IronGolemBundle;
 pub use llama::LlamaBundle;
 pub use panda::PandaBundle;
+pub use piglin::PiglinBundle;
 pub use polar_bear::PolarBearBundle;
 pub use pufferfish::PufferfishBundle;
+pub use spider::SpiderBundle;
+pub use trader_llama::TraderLlamaBundle;
 pub use wolf::WolfBundle;
+pub use zombified_piglin::ZombifiedPiglinBundle;
